f69b2a682ee56fe88869c8d1657db9fd5451f86f
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2024-11-05 01:40:54 -0700

    Time: 851 ms (5.01%) | Memory: 18.1 MB (79.12%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,23 +1,28 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        openCount = []
-        closedCount = 0
+        openCount = 0
         toRemove = []
         for i in range(len(s)):
             if s[i] == ')':
-                if len(openCount) == 0:
+                if openCount == 0:
                     toRemove.append(i)
                 else:
-                    openCount.pop()
+                    openCount-=1
             elif s[i] == '(':
-                openCount.append(i)
-        toRemove.extend(list(openCount))
+                openCount+=1
+        openCount = 0
+        for i in range(len(s)-1,-1,-1):
+            if s[i] == '(':
+                if openCount == 0:
+                    toRemove.append(i)
+                else:
+                    openCount-=1
+            elif s[i] == ')':
+                openCount+=1
         res = ""
         for i in range(len(s)):
             if i not in toRemove:
                 res+=s[i]
-        # for i in toRemove:
-        #     s = s[:i] + s[i+1:]
         return res
 
 
