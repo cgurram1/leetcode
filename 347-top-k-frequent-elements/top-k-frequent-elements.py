5fe8e7b381335b72805a2e9b089e3c99357dc936
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2024-12-19 23:38:15 -0700

    Time: 3 ms (91.91%) | Memory: 21.1 MB (26.87%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,16 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         myDict = {}
+        myHeap = []
         for i in nums:
             if i in myDict:
                 myDict[i] = myDict[i] + 1
             else:
                 myDict[i] = 1
-        sorted_dict_desc = dict(sorted(myDict.items(), key=lambda item: item[1], reverse=True))
+        for key in myDict:
+            heapq.heappush(myHeap,(-myDict[key],key))
         res = []
         count = 0
-        for key in sorted_dict_desc:
-            if count < k:
-                res.append(key)
-                count+=1
+        while(count < k):
+            res.append(heapq.heappop(myHeap)[1])
+            count+=1
         return res
