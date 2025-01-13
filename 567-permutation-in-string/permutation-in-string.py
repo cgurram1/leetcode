6346db77411b249d7d6419827b9e38fb9a3668d3
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-01-13 16:45:55 -0700

    Time: 15 ms (86.02%) | Memory: 18 MB (9.16%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,11 +1,21 @@
+from collections import Counter
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1 = sorted(s1)
+        s1Freq = [0 for _ in range(26)]
+        for ch in s1:
+            s1Freq[ord(ch)-ord('a')]+=1
         l = 0
         r = len(s1)-1
-        while(r < len(s2)):
-            if sorted(s2[l:r+1]) == s1:
+        s2Freq = [0 for _ in range(26)]
+        for ch in s2[l:r+1]:
+            s2Freq[ord(ch)-ord('a')]+=1
+        while(r < len(s2)-1):
+            if s2Freq == s1Freq:
                 return True
+            s2Freq[ord(s2[l])-ord('a')]-=1
             l+=1
             r+=1
+            s2Freq[ord(s2[r])-ord('a')]+=1
+        if s2Freq == s1Freq:
+            return True
         return False
