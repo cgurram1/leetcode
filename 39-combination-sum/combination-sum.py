48ac25cd2e8814c55df57d3c9540dbf868a2d019
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-01-27 13:25:47 -0700

    Time: 14 ms (34.44%) | Memory: 17.8 MB (43.81%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,16 +1,15 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        def myRec(index,currSum,arr):
-            if currSum == target:
+        res = []
+        def myFun(index,arr,s):
+            if s == target:
                 res.append(arr[:])
                 return
-            if index == len(candidates) or currSum > target:
+            if s > target or index == len(candidates):
                 return
             arr.append(candidates[index])
-            myRec(index, currSum + candidates[index],arr)
-            arr.pop()
-            myRec(index + 1, currSum, arr)
-            return
-        res = []
-        myRec(0,0,[])
+            myFun(index, arr,s+candidates[index])
+            arr.pop(-1)
+            myFun(index+1,arr,s)
+        myFun(0,[],0)
         return res
