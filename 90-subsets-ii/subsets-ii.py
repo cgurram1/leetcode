7ee05b5f2c91f4a4ca233b46cbfe02a64109530c
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-01-27 15:53:55 -0700

    Time: 2 ms (30.66%) | Memory: 17.9 MB (40.98%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,14 +1,14 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        def myRec(index,arr):
-            if index == len(nums):
-                if sorted(arr[:]) not in res:
-                    res.append(sorted(arr[:])) 
-                return
-            arr.append(nums[index])
-            myRec(index + 1, arr)
-            arr.pop()
-            myRec(index + 1, arr)
+        nums.sort()
         res = []
-        myRec(0,[])
+        def myFun(index,arr):
+            res.append(arr[:])
+            for j in range(index,len(nums)):
+                if j > index and nums[j] == nums[j-1]:
+                    continue
+                arr.append(nums[j])
+                myFun(j+1,arr)
+                arr.pop(-1)
+        myFun(0,[])
         return res
