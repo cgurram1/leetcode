a94c15bc967a3901e52097c2053891bb2cfd49b7
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-01-27 15:34:15 -0700

    Time: 70 ms (5.05%) | Memory: 17.8 MB (40.79%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        candidates.sort()
+        def myFun(index, arr,s):
+            print(arr)
+            if s == target:
+                res.append(arr[:])
+                return
+            if s > target:
+                return
+            for j in range(index,len(candidates)):
+                if j > index and candidates[j] == candidates[j-1]:
+                    continue
+                arr.append(candidates[j])
+                myFun(j+1,arr,s+candidates[j])
+                arr.pop(-1)
+        myFun(0,[],0)
+        return res
+            
