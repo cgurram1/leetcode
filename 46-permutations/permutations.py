354aab83fb9f0adecccb5685c8a4f4f081e15a6f
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-01-27 16:31:42 -0700

    Time: 0 ms (100.00%) | Memory: 18.1 MB (22.99%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,16 +1,14 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
-        def myFun(arr):
-            if len(arr) == len(nums):
+        def myFun(level,arr):
+            if level == len(arr):
                 res.append(arr[:])
-                return
-            for i in range(len(nums)):
-                if nums[i] not in arr:
-                    arr.append(nums[i])
-                    myFun(arr)
-                    arr.pop(-1)
-        myFun([])
+            for i in range(level,len(nums)):
+                new_arr = arr[:]
+                new_arr[i],new_arr[level] = new_arr[level],new_arr[i]
+                myFun(level + 1,new_arr)
+        myFun(0,nums)
         return res
             
                     
