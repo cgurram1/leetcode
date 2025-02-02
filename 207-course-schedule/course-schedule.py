4be188042999f7ea8e0c9ffd92492f71960fbd43
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-02-02 14:23:09 -0700

    Time: 3 ms (89.22%) | Memory: 18.9 MB (84.64%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -2,20 +2,22 @@ class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         inDegree = [0 for _ in range(numCourses)]
         queue = []
+        adj = [[] for _ in range(numCourses)]
         for arr in prerequisites:
-            inDegree[arr[1]]+=1
+            adj[arr[0]].append(arr[1])
+        for arr in adj:
+            for i in arr:
+                inDegree[i]+=1
         for i in range(numCourses):
             if inDegree[i] == 0:
                 queue.append(i)
         while(queue):
             curr = queue.pop(0)
             numCourses-=1
-            for arr in prerequisites:
-                if arr[0] == curr:
-                    if inDegree[arr[1]] > 0:
-                        inDegree[arr[1]]-=1
-                    if inDegree[arr[1]] == 0:
-                        queue.append(arr[1])
+            for nextNode in adj[curr]:
+                inDegree[nextNode]-=1
+                if inDegree[nextNode] == 0:
+                    queue.append(nextNode)
         if numCourses == 0:
             return True
         return False
