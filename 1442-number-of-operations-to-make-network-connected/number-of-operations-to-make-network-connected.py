ecef2519af23a39ca9c7dfc9748068a55a82004b
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-02-16 15:41:15 -0700

    Time: 39 ms (74.25%) | Memory: 32.8 MB (55.77%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
@@ -1,7 +1,5 @@
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
-        if len(connections) < n - 1:
-            return -1
         parents = [i for i in range(n)]
         rank = [0 for _ in range(n)]
         def findPar(x):
