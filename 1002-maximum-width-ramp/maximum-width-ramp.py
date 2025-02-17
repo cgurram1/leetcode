3d043e5383ff10a9918b259a4f6850c7ffb802c1
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-02-17 09:40:39 -0700

    Time: 74 ms (48.24%) | Memory: 23.8 MB (46.32%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.py
+++ 1002-maximum-width-ramp/maximum-width-ramp.py
@@ -1,26 +1,14 @@
 class Solution:
     def maxWidthRamp(self, nums: List[int]) -> int:
         stack = []
-        stack.append((nums[0],0))
         res = float('-inf')
-        x = -1
         for i in range(len(nums)):
-            if not stack or nums[i] < nums[stack[-1][1]]:
-                stack.append((nums[i],i))
+            if not stack or nums[i] < nums[stack[-1]]:
+                stack.append(i)
         for i in range(len(nums)-1,-1,-1):
-            while(stack and stack[-1][0] <= nums[i]):
+            while(stack and nums[stack[-1]] <= nums[i]):
                 popped = stack.pop(-1)
-            res = max(res,(i - popped[1]))
-        
-        # for i in range(1,len(nums)):
-        #     if nums[i] < stack[-1][0]:
-        #         stack.append((nums[i],i))
-        #         x+=1
-        #     else:
-        #         k = x
-        #         while(k >= 0 and stack[k][0] <= nums[i]):
-        #             k-=1
-        #         res = max(res,(i - stack[k+1][1]))
+            res = max(res,(i - popped))
         if res == float('-inf'):
             return 0
         return res
