b3745c100cc58f2d395c79a2b742acd23405733b
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-02-17 19:11:51 -0700

    Time: 127 ms (59.84%) | Memory: 18.5 MB (11.65%) - LeetSync

--- 3114-beautiful-towers-i/beautiful-towers-i.py
+++ 3114-beautiful-towers-i/beautiful-towers-i.py
@@ -1,8 +1,6 @@
 class Solution:
     def maximumSumOfHeights(self, heights: List[int]) -> int:
         n = len(heights)
-        
-        # Compute left max sum for each index
         left_sum = [0] * n
         stack = []
         for i in range(n):
@@ -13,8 +11,6 @@ class Solution:
             else:
                 left_sum[i] = heights[i] * (i + 1)
             stack.append(i)
-
-        # Compute right max sum for each index
         right_sum = [0] * n
         stack = []
         for i in range(n - 1, -1, -1):
@@ -26,11 +22,7 @@ class Solution:
             else:
                 right_sum[i] = heights[i] * (n - i)
             stack.append(i)
-        print(left_sum)
-        print(right_sum)
-        # Find the maximum sum considering every index as the peak
         max_sum = 0
         for i in range(n):
-            max_sum = max(max_sum, left_sum[i] + right_sum[i] - heights[i])  # Avoid double counting peak
-        
+            max_sum = max(max_sum, left_sum[i] + right_sum[i] - heights[i])
         return max_sum
