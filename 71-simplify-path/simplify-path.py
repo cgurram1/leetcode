35f4ae133afa73de50d4567f1cc64858dc48e7a8
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-02-18 00:35:57 -0700

    Time: 3 ms (37.28%) | Memory: 18 MB (29.22%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -1,48 +1,11 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
         stack = []
-        i = 0
-        if path[-1] != "/":
-            path = path + "/"
-        while(i < len(path)):
-            print(stack)
-            if path[i] == '/':
-                while(i < len(path) and path[i] == '/'):
-                    i+=1
-                i-=1
-                stack.append('/')
-            elif path[i] == '.':
-                dotCount = ""
-                while(i < len(path)):
-                    dotCount += path[i]
-                    if path[i] == '/':
-                        while(i < len(path) and path[i] == "/"):
-                            i+=1
-                        i-=1
-                        break
-                    i+=1
-                if dotCount == "./":
-                    pass
-                elif dotCount == "../":
-                    if stack[-1] != "/":
-                        stack.pop(-1)
-                else:
-                    stack.append(dotCount)
-            else:
-                fileName = ""
-                while(i < len(path) and path[i] != '/'):
-                    fileName += path[i]
-                    i+=1
-                while(i < len(path) and path[i] == "/"):
-                    i+=1
-                i-=1
-                stack.append(fileName + "/")
-            i+=1
-        print(stack)
-        res = ''.join(stack)
-        if len(res) > 1:
-            return res[:-1]
-        elif res == "":
-            return "/"
-        return res
-                
+        components = path.split("/")
+        for comp in components:
+            if comp == "..":
+                if stack:
+                    stack.pop()
+            elif comp and comp != ".":
+                stack.append(comp)
+        return "/" + "/".join(stack)
