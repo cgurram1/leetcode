20be8fcaeb655db8857b3fab3ab358f5838a51a6
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-02-17 19:11:28 -0700

    Time: 137 ms (59.84%) | Memory: 18.3 MB (11.65%) - LeetSync

--- 3114-beautiful-towers-i/beautiful-towers-i.py
+++ 3114-beautiful-towers-i/beautiful-towers-i.py
@@ -1,24 +1,36 @@
 class Solution:
     def maximumSumOfHeights(self, heights: List[int]) -> int:
-        res = float('-inf')
-        def current_max(heights,peak):
-            current_max = heights[peak]
-            res = 0
-            for i in range(peak - 1,-1,-1):
-                if heights[i] <= current_max:
-                    res+=heights[i]
-                    current_max = heights[i]
-                else:
-                    res+=current_max
-            current_max = heights[peak]
-            for i in range(peak + 1,len(heights)):
-                if heights[i] <= current_max:
-                    res+=heights[i]
-                    current_max = heights[i]
-                else:
-                    res+=current_max
-            return res + heights[peak]
-        for i in range(len(heights)):
-            curr = current_max(heights,i)
-            res = max(res,curr)
-        return res
+        n = len(heights)
+        
+        # Compute left max sum for each index
+        left_sum = [0] * n
+        stack = []
+        for i in range(n):
+            while stack and heights[stack[-1]] > heights[i]:
+                stack.pop()
+            if stack:
+                left_sum[i] = left_sum[stack[-1]] + (heights[i] * (i - stack[-1]))
+            else:
+                left_sum[i] = heights[i] * (i + 1)
+            stack.append(i)
+
+        # Compute right max sum for each index
+        right_sum = [0] * n
+        stack = []
+        for i in range(n - 1, -1, -1):
+            print(stack)
+            while stack and heights[stack[-1]] > heights[i]:
+                stack.pop()
+            if stack:
+                right_sum[i] = right_sum[stack[-1]] + (heights[i] * (stack[-1] - i))
+            else:
+                right_sum[i] = heights[i] * (n - i)
+            stack.append(i)
+        print(left_sum)
+        print(right_sum)
+        # Find the maximum sum considering every index as the peak
+        max_sum = 0
+        for i in range(n):
+            max_sum = max(max_sum, left_sum[i] + right_sum[i] - heights[i])  # Avoid double counting peak
+        
+        return max_sum
