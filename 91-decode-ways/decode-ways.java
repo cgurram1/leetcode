f825bd5db4a61322fe89267b045fce3529044e78
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-02-21 11:28:31 -0700

    Time: 10 ms (5.41%) | Memory: 45 MB (5.58%) - LeetSync

---
+++ 91-decode-ways/decode-ways.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int numDecodings(String s) {
+        Map<String,Character> myMap = new HashMap<>();
+        Map<String,Integer> dp = new HashMap<>();
+        Set<String> mySet = new HashSet<>();
+        for(int i = 1;i<27;i++){
+            myMap.put(i+"",(char)(65 + i-1));
+        }
+        return rec("",s,0,myMap,mySet,dp);
+    }
+    public static int rec(String decoded,String next,int index,Map<String,Character> myMap,Set<String> mySet, Map<String,Integer> dp){
+        if(next == ""){
+            if(mySet.contains(decoded) == false){
+                mySet.add(decoded);
+                return 1;
+            }
+            return 0;
+        }
+        if(dp.containsKey(next)){
+            return dp.get(next);
+        }
+        int one = 0;
+        int two = 0;
+        if(myMap.containsKey("" + next.charAt(0))){
+            one = rec(decoded + myMap.get("" + next.charAt(0)),next.substring(1,next.length()),index+1,myMap,mySet,dp);
+        }
+        if(next.length() > 1){
+            if(myMap.containsKey(next.charAt(0) + "" + next.charAt(1))){
+                two = rec(decoded + myMap.get(next.charAt(0) + "" + next.charAt(1)),next.substring(2,next.length()),index+2,myMap,mySet,dp);
+            }
+        }
+        dp.put(next,one + two);
+        return(one + two);
+    }
+}
