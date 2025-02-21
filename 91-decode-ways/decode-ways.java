5e540da37a8444c8613ef781a43d8404491ebca8
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-02-21 12:05:45 -0700

    Time: 7 ms (5.41%) | Memory: 42.7 MB (5.58%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,19 +1,22 @@
 class Solution {
     public int numDecodings(String s) {
-        int[] dp = new int[s.length()+1];
-        dp[s.length()] = 1;
+        // int[] dp = new int[s.length()+1];
+        // dp[s.length()] = 1;
+        int next_first=1, next_second=0;
         for(int index = s.length()-1;index>=0;index--){
             int one = 0,two = 0;
             if(Integer.valueOf(s.substring(index, index + 1)) > 0 && Integer.valueOf(s.substring(index, index + 1)) < 10){
-                one = dp[index+1];
+                one = next_first;
             }
             if(index + 1 < s.length()){
                 if(Integer.valueOf(s.substring(index, index + 1)) != 0 && Integer.valueOf(s.charAt(index) + "" + s.charAt(index + 1)) <= 26){
-                    two = dp[index + 2];
+                    two = next_second;
                 }
             }
-            dp[index] = one + two;
+            // dp[index] = one + two;
+            next_second=next_first;
+            next_first=one+two;
         }
-        return dp[0];
+        return next_first;
     }
 }
