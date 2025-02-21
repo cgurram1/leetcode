4cddc7ccba702ae06d47a62cfba3b685a5573e4d
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-02-21 15:26:42 -0700

    Time: 1 ms (69.36%) | Memory: 41.6 MB (78.38%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
@@ -1,29 +1,16 @@
 class Solution {
     public String smallestNumber(String pattern) {
-        int curr = 1;
-        int dcount = 0;
-        int [] darr = new int[pattern.length()];
+        int curr = 0;
         String s = "";
-        for(int i = pattern.length()-1;i>=0;i--){
-            if(pattern.charAt(i) == 'D'){
-                dcount+=1;
-            }
-            else{
-                dcount = 0;
-            }
-            darr[i] = dcount;
-        }
-        for(int i = 0;i<darr.length;i++){
-            if(darr[i] == 0){
-                s+=curr;
-                curr = curr + dcount + 1;
-                dcount = 0;
-            }
-            else{
-                dcount = Math.max(dcount,darr[i]);
-                s+=(darr[i] + curr);
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0;i<=pattern.length();i++){
+            st.push(i+1);
+            if(i == pattern.length() || pattern.charAt(i) == 'I'){
+                while(st.isEmpty() == false){
+                    s+=st.pop();
+                }
             }
         }
-        return s + curr;
+        return s;
     }
 }
