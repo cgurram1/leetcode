47cd6555e82d9ea004f5cc8b22ab01b308a8c824
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-02-21 11:34:03 -0700

    Time: 9 ms (5.41%) | Memory: 45 MB (5.58%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -2,19 +2,15 @@ class Solution {
     public int numDecodings(String s) {
         Map<String,Character> myMap = new HashMap<>();
         Map<String,Integer> dp = new HashMap<>();
-        Set<String> mySet = new HashSet<>();
+        // Set<String> mySet = new HashSet<>();
         for(int i = 1;i<27;i++){
             myMap.put(i+"",(char)(65 + i-1));
         }
-        return rec("",s,0,myMap,mySet,dp);
+        return rec("",s,0,myMap,dp);
     }
-    public static int rec(String decoded,String next,int index,Map<String,Character> myMap,Set<String> mySet, Map<String,Integer> dp){
+    public static int rec(String decoded,String next,int index,Map<String,Character> myMap, Map<String,Integer> dp){
         if(next == ""){
-            if(mySet.contains(decoded) == false){
-                mySet.add(decoded);
-                return 1;
-            }
-            return 0;
+            return 1;
         }
         if(dp.containsKey(next)){
             return dp.get(next);
@@ -22,11 +18,11 @@ class Solution {
         int one = 0;
         int two = 0;
         if(myMap.containsKey("" + next.charAt(0))){
-            one = rec(decoded + myMap.get("" + next.charAt(0)),next.substring(1,next.length()),index+1,myMap,mySet,dp);
+            one = rec(decoded + myMap.get("" + next.charAt(0)),next.substring(1,next.length()),index+1,myMap,dp);
         }
         if(next.length() > 1){
             if(myMap.containsKey(next.charAt(0) + "" + next.charAt(1))){
-                two = rec(decoded + myMap.get(next.charAt(0) + "" + next.charAt(1)),next.substring(2,next.length()),index+2,myMap,mySet,dp);
+                two = rec(decoded + myMap.get(next.charAt(0) + "" + next.charAt(1)),next.substring(2,next.length()),index+2,myMap,dp);
             }
         }
         dp.put(next,one + two);
