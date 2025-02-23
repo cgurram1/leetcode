5127e8baaa6018d6f0d8281c3d8e6ea9f18200e6
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-02-23 15:39:10 -0700

    Time: 170 ms (38.29%) | Memory: 18 MB (48.50%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,19 +1,25 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        res = 1
-        freq = [0] * 26
-        left = 0
-        right = 0
-        freq[ord(s[right]) - 65]+=1
-        while(right < len(s)):
-            window_len = right - left + 1
-            if window_len - max(freq) <= k:
-                right+=1
-                if right < len(s):
-                    freq[ord(s[right]) - 65]+=1
+        myDict = {}
+        l = 0
+        r = 0
+        max_freq = 0
+        currSum = 0
+        res = 0
+        while(r < len(s)):
+            if s[r] in myDict:
+                myDict[s[r]]+=1
             else:
-                res = max(res,window_len-1)
-                freq[ord(s[left]) - 65]-=1
-                left+=1
-        return max(res,window_len)
-                
+                myDict[s[r]] = 1
+            max_freq = max(myDict.values())
+            if r - l + 1 - max_freq > k:
+                if myDict[s[l]] == 1:
+                    del myDict[s[l]]
+                else:
+                    myDict[s[l]]-=1
+                max_freq = max(myDict.values())
+                l+=1
+            if r - l + 1 - max_freq <= k:
+                res = max(res,r - l + 1)
+            r+=1
+        return res
