32102828c072cc3d9ccb59c4746d2ef1c04b1564
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-02-23 08:27:58 -0700

    Time: 7 ms (14.70%) | Memory: 17.9 MB (28.54%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.py
+++ 316-remove-duplicate-letters/remove-duplicate-letters.py
@@ -1,14 +1,18 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
         stack = []
-        myDict = {char: s.count(char) for char in s}
-        inStack = set()
-        for char in s:
-            myDict[char] -= 1 
-            if char in inStack:
+        myDict = {}
+        res = ""
+        for i in range(len(s)):
+            if s[i] in myDict:
+                myDict[s[i]]+=1
+            else:
+                myDict[s[i]] = 1
+        for i in range(len(s)):
+            myDict[s[i]]-=1
+            if s[i] in stack:
                 continue
-            while stack and char < stack[-1] and myDict[stack[-1]] > 0:
-                inStack.remove(stack.pop())
-            stack.append(char)
-            inStack.add(char)
+            while (stack and ord(s[i]) < ord(stack[-1]) and myDict[stack[-1]] > 0):
+                popped = stack.pop(-1)
+            stack.append(s[i])
         return "".join(stack)
