ac3bfb4f28ad04004a366da11bf96298590bfd4a
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-02-23 21:28:04 -0700

    Time: 127 ms (78.14%) | Memory: 17.7 MB (65.41%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -3,18 +3,22 @@ class Solution:
         l = 0
         r = 0
         count = 0
-        curr = ""
-        dummy_str = ""
-        for ch in s:
-            dummy_str+=(ch + "*")
-        dummy_str = dummy_str[:-1]
-        for i in range(len(dummy_str)):
+        for i in range(len(s)):
             l = i
             r = i
-            while(l >= 0 and r<len(dummy_str)):
-                if dummy_str[l] == dummy_str[r]:
-                    if dummy_str[l] != "*":
-                        count+=1
+            while(l >= 0 and r<len(s)):
+                if s[l] == s[r]:
+                    count+=1
+                else:
+                    break
+                l-=1
+                r+=1
+        for i in range(len(s)):
+            l = i
+            r = i+1
+            while(l >= 0 and r<len(s)):
+                if s[l] == s[r]:
+                    count+=1
                 else:
                     break
                 l-=1
