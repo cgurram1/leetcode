6fac53435e779c125990d0d4188b2c984ba54299
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-02-25 09:57:39 -0700

    Time: 1 ms (99.79%) | Memory: 44.6 MB (22.60%) - LeetSync

---
+++ 120-triangle/triangle.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int [][] dp = new int[triangle.size()][triangle.size()];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        return rec(0,0,triangle,dp);
+    }
+    public int rec(int i,int j,List<List<Integer>> triangle,int [][] dp){
+        if(i == triangle.size()-1){
+            return triangle.get(i).get(j);
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j] + triangle.get(i).get(j);
+        }
+        dp[i][j] = Math.min(rec(i+1,j,triangle,dp),rec(i+1,j+1,triangle,dp));
+        return triangle.get(i).get(j) + dp[i][j];
+    }
+}
