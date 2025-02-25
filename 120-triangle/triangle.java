fc5757234c5559128f5ca777b670ff4028ae1c25
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-02-25 09:58:14 -0700

    Time: 1 ms (99.79%) | Memory: 44.2 MB (54.18%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -11,9 +11,9 @@ class Solution {
             return triangle.get(i).get(j);
         }
         if(dp[i][j] != -1){
-            return dp[i][j] + triangle.get(i).get(j);
+            return dp[i][j];
         }
-        dp[i][j] = Math.min(rec(i+1,j,triangle,dp),rec(i+1,j+1,triangle,dp));
-        return triangle.get(i).get(j) + dp[i][j];
+        dp[i][j] = triangle.get(i).get(j) + Math.min(rec(i+1,j,triangle,dp),rec(i+1,j+1,triangle,dp));
+        return dp[i][j];
     }
 }
