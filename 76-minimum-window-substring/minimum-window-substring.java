2a1c9249136e73efa632c4b7198c7c203a907a05
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-03-12 17:14:51 -0700

    Time: 114 ms (17.72%) | Memory: 45.8 MB (12.21%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,50 +1,33 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(t.length() > s.length()){
-            return "";
-        }
-        Map<Character,Integer> map = new HashMap<>();
-        int lmin = Integer.MAX_VALUE;
-        int rmin = Integer.MAX_VALUE;
-        int r = 0;
-        int l = 0;
-        int count = 0;
-        int res = s.length()+1;
+        HashMap<Character,Integer> map = new HashMap<>();
         for(int i = 0;i<t.length();i++){
-            if(map.containsKey(t.charAt(i))){
-                map.put(t.charAt(i),map.get(t.charAt(i))+ 1);
-            }
-            else{
-                map.put(t.charAt(i),1);
-            }
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) + 1);
         }
-        while(r < s.length()){
-            if(map.containsKey(s.charAt(r))){
-                map.put(s.charAt(r),map.get(s.charAt(r)) - 1);
-            }
-            else{
-                map.put(s.charAt(r),-1);
-            }
-            if(map.get(s.charAt(r)) >= 0){
+        int left = 0;
+        int right = 0;
+        int count = 0;
+        int result = s.length()+1;
+        String res = "";
+        while(right < s.length()){
+            map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0) - 1);
+            if(map.get(s.charAt(right)) >= 0){
                 count+=1;
             }
             while(count == t.length()){
-                if( r - l + 1 < res){
-                    lmin = l;
-                    rmin = r;
-                    res = r - l + 1;
+                if(right - left + 1 < result){
+                    res = s.substring(left,right+1);
+                    result = right - left + 1;
                 }
-                map.put(s.charAt(l),map.get(s.charAt(l)) + 1);
-                if(map.get(s.charAt(l)) > 0){
+                map.put(s.charAt(left), map.get(s.charAt(left)) + 1);
+                if(map.get(s.charAt(left)) > 0){
                     count-=1;
                 }
-                l+=1;
+                left+=1;
             }
-            r+=1;
+            
+            right+=1;
         }
-        if(lmin == Integer.MAX_VALUE){
-            return "";
-        }
-        return s.substring(lmin,rmin+1);
+        return res;
     }
 }
