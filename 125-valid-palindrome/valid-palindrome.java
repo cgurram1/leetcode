2f4bf5a87e8f8d8239443fc3d06b3952530b51ec
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-03-14 09:03:43 -0700

    Time: 2 ms (99.08%) | Memory: 43.2 MB (70.79%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,18 +1,23 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder sb = new StringBuilder();
-        s = s.replaceAll(" ","").toLowerCase();
-        for(int i = 0;i<s.length();i++){
-            if((s.charAt(i) >= 'a' && s.charAt(i) <= 'z') || (s.charAt(i) >= '0' && s.charAt(i) <= '9')){
-                sb.append(s.charAt(i));
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            // Move left pointer until it finds an alphanumeric character
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
             }
-        }
-        int left = 0;
-        int right = sb.length()-1;
-        while(left < right){
-            if(sb.charAt(left) != sb.charAt(right)){
+
+            // Move right pointer until it finds an alphanumeric character
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            // Compare characters (ignore case)
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
                 return false;
             }
+
             left++;
             right--;
         }
