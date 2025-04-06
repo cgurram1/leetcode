aae551e52b104ff4272fce6e62ad5e34ad0d3fdf
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-04-06 12:22:50 -0700

    Time: 5 ms (49.46%) | Memory: 4.8 MB (72.38%) - LeetSync

--- 242-valid-anagram/valid-anagram.go
+++ 242-valid-anagram/valid-anagram.go
@@ -1,9 +1,28 @@
 func isAnagram(s string, t string) bool {
-    charsS := strings.Split(s,"")
-    sort.Strings(charsS)
-    newS := strings.Join(charsS,"")
-    charsT := strings.Split(t,"")
-    sort.Strings(charsT)
-    newT := strings.Join(charsT,"")
-    return newS == newT
+    // charsS := strings.Split(s,"")
+    // sort.Strings(charsS)
+    // newS := strings.Join(charsS,"")
+    // charsT := strings.Split(t,"")
+    // sort.Strings(charsT)
+    // newT := strings.Join(charsT,"")
+    // return newS == newT
+
+
+    sMap := make(map[rune]int)
+    tMap := make(map[rune]int)
+    for _,char := range s{
+        sMap[char]+=1
+    }
+    for _,char := range t{
+        tMap[char]+=1
+    }
+    if len(sMap) != len(tMap){
+        return false
+    }
+    for key,val := range sMap{
+        if tMap[key] != val{
+            return false
+        }
+    }
+    return true
 }
