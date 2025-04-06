7e812b8f70ffa8b2fe49ec50bf80036104bd3795
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-04-06 12:37:29 -0700

    Time: 3 ms (44.08%) | Memory: 5.9 MB (40.08%) - LeetSync

--- 1-two-sum/two-sum.go
+++ 1-two-sum/two-sum.go
@@ -1,32 +1,14 @@
-import "sort"
-import "fmt"
 func twoSum(nums []int, target int) []int {
-    hash := make(map[int][]int)
-    for i, v := range nums {
-        if _, ok := hash[v]; !ok {
-            hash[v] = []int{i}
-        } else {
-            hash[v] = append(hash[v], i)
+    var result []int
+    myMap := make(map[int]int)
+    for index,num := range nums{
+        value,ok := myMap[target - num]
+        if(ok == true){
+            result = append(result,index)
+            result = append(result,value)
+            return result
         }
+        myMap[num] = index
     }
-    sortedNums := make([]int, len(nums))
-    copy(sortedNums, nums)
-    sort.Ints(sortedNums)
-    left := 0
-    right := len(sortedNums) - 1
-    for left < right {
-        s := sortedNums[left] + sortedNums[right]
-        if s == target {
-            if sortedNums[left] == sortedNums[right] {
-                return hash[sortedNums[left]]
-            } else {
-                return []int{hash[sortedNums[left]][0], hash[sortedNums[right]][0]}
-            }
-        } else if s < target {
-            left++
-        } else {
-            right--
-        }
-    }
-    return []int{}
+    return result
 }
