29dd62ff7b7cc493308aa17408fd1668c186e264
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-04-18 22:11:09 -0700

    Time: 8 ms (68.56%) | Memory: 45.2 MB (18.37%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,8 +1,16 @@
 class Solution {
-    public int findTargetSumWays(int[] nums, int target) {
-        return fun(0,0,nums,target);
+    public int findTargetSumWays(int[] nums, int target){
+        int mathSum = 0;
+        for(int i = 0;i<nums.length;i++){
+            mathSum += nums[i];
+        }
+        int [][] dp = new int[nums.length + 1][mathSum * 2 + 1];
+        for(int [] arr : dp){
+            Arrays.fill(arr,-1);
+        }
+        return fun(0,0,nums,target,dp,mathSum);
     }
-    public int fun(int index, int sum, int [] nums,int target){
+    public int fun(int index, int sum, int [] nums,int target, int [][] dp,int mathSum){
         if(index == nums.length){
             if(sum == target){
                 return 1;
@@ -11,6 +19,10 @@ class Solution {
                 return 0;
             }
         }
-        return fun(index + 1, sum + nums[index],nums,target) + fun(index + 1, sum - nums[index],nums,target);
+        if(dp[index][sum + mathSum] != -1){
+            return dp[index][sum + mathSum];
+        }
+        dp[index][sum + mathSum] = fun(index + 1, sum + nums[index],nums,target,dp,mathSum) + fun(index + 1, sum - nums[index],nums,target,dp,mathSum);
+        return dp[index][sum + mathSum];
     }
 }
