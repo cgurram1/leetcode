e3a093ca947c7083996fff763bbad05002e8bb7b
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-04-29 22:41:31 -0700

    Time: 30 ms (14.95%) | Memory: 50.8 MB (67.28%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,26 +1,27 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int [][] dp = new int[text1.length()][text2.length()];
-        for(int [] row : dp){
-            Arrays.fill(row,-1);
+        int [][] dp = new int[text1.length()+1][text2.length()+1];
+        for(int [] arr : dp){
+            Arrays.fill(arr,-1);
         }
-        return rec(0,0,text1,text2,dp);
+        return fun(0,0,text1,text2,dp);
     }
-    public static int rec(int i,int j,String text1,String text2,int [][] dp){
-        if(i > text1.length() - 1 || j > text2.length() - 1){
+    public int fun(int index1, int index2,String text1, String text2,int [][] dp){
+        if(index1 >= text1.length() || index2 >= text2.length()){
             return 0;
         }
-        if(dp[i][j] != -1){
-            return dp[i][j];
+        if(dp[index1][index2] != -1){
+            return dp[index1][index2];
         }
-        int res = 0;
-        if(text1.charAt(i) == text2.charAt(j)){
-            return 1 + rec(i + 1,j + 1,text1,text2,dp);
+        if(text1.charAt(index1) == text2.charAt(index2)){
+            dp[index1][index2] = 1 + fun(index1 + 1, index2 + 1,text1,text2,dp); 
+            return dp[index1][index2];
         }
         else{
-            res = Math.max(res, Math.max(rec(i,j+1,text1,text2,dp),rec(i+1,j,text1,text2,dp)));
+            int moveLeft = fun(index1 + 1, index2,text1,text2,dp);
+            int moveRight = fun(index1, index2 + 1,text1,text2,dp);
+            dp[index1][index2] = Math.max(moveLeft, moveRight);
+            return dp[index1][index2];
         }
-        dp[i][j] = res;
-        return dp[i][j];
     }
 }
