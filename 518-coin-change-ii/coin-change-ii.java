aa146773b8e35bd8d5245630765e958df73859e1
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-04-29 22:12:25 -0700

    Time: 35 ms (6.13%) | Memory: 51.6 MB (7.89%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,28 +1,15 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int [][] dp = new int[coins.length+1][amount+1];
-        for(int [] arr : dp){
-            Arrays.fill(arr,-1);
-        }
-        return fun(0,0,amount, coins,dp);
-    }
-    public int fun(int index, int cost, int amount, int [] coins,int [][] dp){
-        if(index == coins.length){
-            return 0;
-        }
-        if(cost == amount){
-            return 1;
-        }
-        if(dp[index][cost] != -1){
-            return dp[index][cost];
-        }
-        int pick = 0;
-        if(cost + coins[index] <= amount){
-            pick = fun(index, cost + coins[index], amount, coins,dp);
-        }
-        int notPick = fun(index + 1, cost,amount, coins,dp);
-        dp[index][cost] = pick + notPick;
-        return dp[index][cost];
+        Integer[][] dp = new Integer[coins.length][amount + 1];
+        return dfs(0, amount, coins, dp);
     }
 
+    public int dfs(int index, int remaining, int[] coins, Integer[][] dp) {
+        if (remaining == 0) return 1;
+        if (index == coins.length || remaining < 0) return 0;
+        if (dp[index][remaining] != null) return dp[index][remaining];
+        int pick = dfs(index, remaining - coins[index], coins, dp);
+        int notPick = dfs(index + 1, remaining, coins, dp);
+        return dp[index][remaining] = pick + notPick;
+    }
 }
