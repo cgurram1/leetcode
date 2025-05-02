9dbcf3c16a4d085923c8fa38fd4401cd8a58e886
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-05-01 22:31:13 -0700

    Time: 12 ms (5.12%) | Memory: 44.4 MB (81.82%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -6,7 +6,7 @@ class Solution {
         for(int i = 0;i<grid.length;i++){
             for(int j = 0;j<grid[0].length;j++){
                 if(grid[i][j] == 1){
-                    result = Math.max(result,dfs(i,j,grid) + 1);
+                    result = Math.max(result,dfs(i,j,grid));
                     System.out.println(result);
                 }
             }   
@@ -15,13 +15,13 @@ class Solution {
     }
     public int dfs(int x,int y,int [][]grid) {
         grid[x][y] = 0;
-        int count = 0;
+        int count = 1;
         for(int k = 0;k<4;k++){
             int nextx = x + xNext[k];
             int nexty = y + yNext[k];
             if(nextx >= 0 && nexty >= 0 && nextx < grid.length && nexty < grid[0].length && grid[nextx][nexty] == 1){
                 
-                count = count + 1 + dfs(nextx,nexty,grid);
+                count = count + dfs(nextx,nexty,grid);
             }
         }
         return count;
