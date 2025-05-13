2d13393cec33cf6edf59659255c97d5a5feb1dd8
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-05-13 10:59:21 -0700

    Time: 248 ms (35.44%) | Memory: 45.6 MB (53.16%) - LeetSync

---
+++ 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int lengthAfterTransformations(String s, int t) {
+        int[] freq = new int[26];
+        int result = s.length();
+        int MOD = 1000000007;
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+
+        for (int k = 0; k < t; k++) {
+            int[] newFreq = new int[26];
+            for (int i = 0; i < 26; i++) {
+                if (freq[i] > 0) {
+                    if (i == 25) {
+                        newFreq[0] = (int)(((long)newFreq[0] + freq[i]) % MOD);
+                        newFreq[1] = (int)(((long)newFreq[1] + freq[i]) % MOD);
+                        result = (int)(((long)result + freq[i]) % MOD);
+                    } else {
+                        newFreq[i + 1] = (int)(((long)newFreq[i + 1] + freq[i]) % MOD);
+                    }
+                }
+            }
+            freq = newFreq;
+        }
+
+        return result;
+    }
+}
