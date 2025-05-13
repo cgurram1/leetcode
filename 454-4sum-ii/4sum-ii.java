a8518340b49111c9b3606c7ccb3f49abb5a817ff
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-05-13 16:30:27 -0700

    Time: 109 ms (82.21%) | Memory: 44.9 MB (65.71%) - LeetSync

--- 454-4sum-ii/4sum-ii.java
+++ 454-4sum-ii/4sum-ii.java
@@ -1,24 +1,16 @@
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i = 0;i<nums1.length;i++){
-            for(int j = 0;j<nums2.length;j++){
-                int sum = nums1[i] + nums2[j];
-                if(map.containsKey(sum)){
-                    map.put(sum, map.get(sum) + 1);
-                }
-                else{
-                    map.put(sum, 1);
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int a : nums1) {
+            for (int b : nums2) {
+                map.put(a + b, map.getOrDefault(a + b, 0) + 1);
             }
         }
         int res = 0;
-        for(int i = 0;i<nums3.length;i++){
-            for(int j = 0;j<nums4.length;j++){
-                int sum = nums3[i] + nums4[j];
-                if(map.containsKey(-1*sum)){
-                    res += map.get(-1*sum);
-                }
+        for (int c : nums3) {
+            for (int d : nums4) {
+                res += map.getOrDefault(-(c + d), 0);
             }
         }
         return res;
