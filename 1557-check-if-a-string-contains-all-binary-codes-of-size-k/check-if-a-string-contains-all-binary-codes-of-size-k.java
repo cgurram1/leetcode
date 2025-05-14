bfc4443a056a46ab0822a8001df4e2e89a0cd40b
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-05-14 06:18:52 -0700

    Time: 164 ms (50.47%) | Memory: 68.8 MB (48.12%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -1,32 +1,11 @@
 class Solution {
     // BruteForce
     //Generate all k length binary strings anc check if it exists in the set
-
-
     public boolean hasAllCodes(String s, int k) {
         Set<String> set = new HashSet<>();
-        // StringBuilder st = new StringBuilder();
-        // for(int i = 0;i<k;i++){
-        //     st.append("0");
-        // }
         for(int i = 0;i<=s.length()-k;i++){
             set.add(s.substring(i,i+k));
         }
         return set.size() == Math.pow(2,k);
-    }
-    public boolean fun(int index,StringBuilder st,Set<String> set){
-        if(index == st.length()){
-            if(set.contains(st.toString())){
-                return true;
-            }
-            return false;
-        }
-        // keep it 0
-        boolean zero = fun(index + 1, st,set);
-        // change to 1
-        st.setCharAt(index,'1');
-        boolean one = fun(index + 1,st,set);
-        st.setCharAt(index,'0');
-        return zero && one;
     }
 }
