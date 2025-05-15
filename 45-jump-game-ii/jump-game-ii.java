442952663789192cf53d31cfc1233ad9261db525
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-05-15 12:19:39 -0700

    Time: 123 ms (7.41%) | Memory: 45.7 MB (18.30%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,17 +1,22 @@
 class Solution {
     public int jump(int[] nums) {
-        int jumps = 0;
-        int left = 0;
-        int right = 0;
-        while(right < nums.length-1){
-            int farthest = 0;
-            for(int i = left;i<=right;i++){
-                farthest = Math.max(farthest, i + nums[i]);
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        return dfs(0, nums, dp);
+    }
+
+    public int dfs(int index, int[] nums, int[] dp) {
+        if (index >= nums.length - 1) return 0;
+        if (dp[index] != -1) return dp[index];
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i <= nums[index]; i++) {
+            int next = dfs(index + i, nums, dp);
+            if (next != Integer.MAX_VALUE) {
+                min = Math.min(min, 1 + next);
             }
-            left = right + 1;
-            right = farthest;
-            jumps++;
         }
-        return jumps;
+
+        return dp[index] = min;
     }
 }
