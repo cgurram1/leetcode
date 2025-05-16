43658c56de71a1a180ca1bd7350917e6d50415bc
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-05-15 17:21:18 -0700

    Time: 0 ms (100.00%) | Memory: 41.4 MB (18.43%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -1,20 +1,23 @@
 class Solution {
+    List<List<Integer>> result = new ArrayList<>();
     public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> result = new ArrayList<>();
-        fun(1,new ArrayList<>(), 0,n,result,k);
-        return result;
+        fun(0,0,new ArrayList<>(),n,k);
+        return this.result;
     }
-    public void fun(int index, List<Integer> path, int sum,int n, List<List<Integer>> result, int k){
-        if(sum == n && path.size() == k){
-            result.add(new ArrayList<>(path));
+    public void fun(int index,int sum,List<Integer> curr,int n,int k){
+        if(sum > n || curr.size() > k){
             return;
         }
-        if(index > 9){
+        if(sum == n){
+            if(curr.size() == k){
+                this.result.add(new ArrayList<>(curr));
+            }
             return;
         }
-        path.add(index);
-        fun(index+1, path, sum + index, n,result,k);
-        path.remove(path.size()-1);
-        fun(index + 1, path, sum,n,result,k);
+        for(int i = index+1;i<=9;i++){
+            curr.add(i);
+            fun(i, sum + i,curr,n,k);
+            curr.remove(curr.size()-1);
+        }
     }
 }
