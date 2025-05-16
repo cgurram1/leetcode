3e6926b0029a4623a8e71876e78cf82f55ee809a
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-05-16 10:12:24 -0700

    Time: 361 ms (14.28%) | Memory: 77.1 MB (19.14%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,23 +1,30 @@
+import java.util.Arrays;
+
 class Solution {
-    private int[][] dp;
-    private int[] nums;
+    public int[] nums;
+    public int[][] dp;
 
     public int lengthOfLIS(int[] nums) {
         this.nums = nums;
         int n = nums.length;
         dp = new int[n][n + 1];
-        for (int[] row : dp) Arrays.fill(row, -1);
+        for (int[] arr : dp) {
+            Arrays.fill(arr, -1);
+        }
         return fun(0, -1);
     }
-    private int fun(int index, int prevIndex) {
+
+    public int fun(int index, int prevIndex) {
         if (index == nums.length) return 0;
-        if (dp[index][prevIndex + 1] != -1) return dp[index][prevIndex + 1];
-        int taken = 0;
-        if (prevIndex == -1 || nums[index] > nums[prevIndex]) {
-            taken = 1 + fun(index + 1, index);
+        if (dp[index][prevIndex + 1] != -1) {
+            return dp[index][prevIndex + 1];
         }
-        int notTaken = fun(index + 1, prevIndex);
-        dp[index][prevIndex + 1] = Math.max(taken, notTaken);
+        int pick = 0;
+        if (prevIndex == -1 || nums[index] > nums[prevIndex]) {
+            pick = 1 + fun(index + 1, index);
+        }
+        int notPick = fun(index + 1, prevIndex);
+        dp[index][prevIndex + 1] = Math.max(pick, notPick);
         return dp[index][prevIndex + 1];
     }
 }
