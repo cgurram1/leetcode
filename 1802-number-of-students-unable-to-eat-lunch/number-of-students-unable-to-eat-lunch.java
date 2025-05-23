dac35634211d799d5e213e3cdf3bab7a00d426ed
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-05-22 17:44:45 -0700

    Time: 0 ms (100.00%) | Memory: 41.7 MB (49.72%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.java
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.java
@@ -3,16 +3,6 @@ class Solution {
         Queue<Integer> queue = new LinkedList<>();
         int onesStudent = 0;
         int zerosStudent = 0;
-        int onesPizza = 0;
-        int zerosPizza = 0;
-        for(int i = 0;i<sandwiches.length;i++){
-            if(sandwiches[i] == 1){
-                onesPizza+=1;
-            }
-            else{
-                zerosPizza+=1;
-            }
-        }
         for(int i = 0;i<students.length;i++){
             queue.add(students[i]);
             if(students[i] == 1){
@@ -22,28 +12,15 @@ class Solution {
                 zerosStudent+=1;
             }
         }
-        int pizza = 0;
-        while(!queue.isEmpty()){
-            System.out.println(queue);
-            if(queue.peek() == sandwiches[pizza]){
-                if(queue.peek() == 1){
-                    onesStudent-=1;
-                    onesPizza-=1;
-                }
-                else{
-                    zerosStudent-=1;
-                    zerosPizza-=1;
-                }
-                queue.poll();
-                pizza+=1;
-            }
-            else{
-                if(sandwiches[pizza] == 1 && onesStudent == 0 || sandwiches[pizza] == 0 && zerosStudent == 0){
-                    break;
-                }
-                queue.add(queue.poll());
+        for (int i = 0; i < sandwiches.length; i++) {
+            if (sandwiches[i] == 1) {
+                if (onesStudent > 0) onesStudent--;
+                else break;
+            } else {
+                if (zerosStudent > 0) zerosStudent--;
+                else break;
             }
         }
-        return queue.size();
+        return onesStudent + zerosStudent;
     }
 }
