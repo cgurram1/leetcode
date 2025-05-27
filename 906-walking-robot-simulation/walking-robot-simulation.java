e674356eb8d02ae35302c0f78ffcfeec9af545f2
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-05-26 22:01:21 -0700

    Time: 36 ms (42.98%) | Memory: 49.3 MB (72.34%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.java
+++ 906-walking-robot-simulation/walking-robot-simulation.java
@@ -4,46 +4,74 @@ class Solution {
         int x = 0;
         int y = 0;
         int dir = 0;
+        Set<String> obsSet = new HashSet<>();
+            for (int[] obs : obstacles) {
+                obsSet.add(obs[0] + "," + obs[1]);
+            }
         for(int i = 0;i<commands.length;i++){
             if(commands[i] > 0){
                 if(dir == 0){
                     for(int k = 0;k<commands[i];k++){
-                        if(isObstacle(y+1,x,obstacles) == false){
-                            y+=1;
-                        }
-                        else{
+                        if(obsSet.contains(x + "," + (y+1))){
                             continue;
                         }
+                        else{
+                            y+=1;
+                        }
+                        // if(isObstacle(y+1,x,obstacles) == false){
+                        //     y+=1;
+                        // }
+                        // else{
+                        //     continue;
+                        // }
                     }
                 }
                 else if(dir == 1){
                     for(int k = 0;k<commands[i];k++){
-                        if(isObstacle(y,x+1,obstacles) == false){
-                            x+=1;
-                        }
-                        else{
+                        if(obsSet.contains(x+1 + "," + (y))){
                             continue;
                         }
+                        else{
+                            x+=1;
+                        }
+                        // if(isObstacle(y,x+1,obstacles) == false){
+                        //     x+=1;
+                        // }
+                        // else{
+                        //     continue;
+                        // }
                     }
                 }
                 else if(dir == 2){
                     for(int k = 0;k<commands[i];k++){
-                        if(isObstacle(y,x-1,obstacles) == false){
-                            x-=1;
-                        }
-                        else{
+                        if(obsSet.contains(x-1 + "," + (y))){
                             continue;
                         }
+                        else{
+                            x-=1;
+                        }
+                        // if(isObstacle(y,x-1,obstacles) == false){
+                        //     x-=1;
+                        // }
+                        // else{
+                        //     continue;
+                        // }
                     }
                 }
                 else{
                     for(int k = 0;k<commands[i];k++){
-                        if(isObstacle(y-1,x,obstacles) == false){
-                            y-=1;
-                        }
-                        else{
+                        if(obsSet.contains(x + "," + (y-1))){
                             continue;
                         }
+                        else{
+                            y-=1;
+                        }
+                        // if(isObstacle(y-1,x,obstacles) == false){
+                        //     y-=1;
+                        // }
+                        // else{
+                        //     continue;
+                        // }
                     }
                 }
             }
@@ -83,7 +111,6 @@ class Solution {
             }
             result = Math.max(result,x*x + y*y);
         }
-        // System.out.println(x + " " + y);
         return result;
     }
     public boolean isObstacle(int y,int x,int[][] obstacles){
