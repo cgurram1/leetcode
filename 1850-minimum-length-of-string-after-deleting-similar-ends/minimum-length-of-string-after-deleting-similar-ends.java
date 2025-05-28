0bd2bf20c5ff7074a6785bf294b79f2283b69318
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-05-27 22:04:45 -0700

    Time: 6 ms (15.98%) | Memory: 45.2 MB (80.40%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.java
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.java
@@ -1,12 +1,26 @@
 class Solution {
     public int minimumLength(String s) {
-        char[] a = s.toCharArray();
-        int l = 0, r = a.length - 1;
-        while (l < r && a[l] == a[r]) {
-            char c = a[l];
-            while (l <= r && a[l] == c) l++;
-            while (l <= r && a[r] == c) r--;
+        int left = 0;
+        int right = s.length()-1;
+        while(left <= right && s.charAt(left) == s.charAt(right)){
+            if(left + 1 == right && s.charAt(left) == s.charAt(right)){
+                return 0;
+            }
+            if(left == right){
+                return 1;
+            }
+            while(left + 1 <= right && s.charAt(left) == s.charAt(left+1)){
+                left+=1;
+            }
+            if(left + 1 > right){
+                return 0;
+            }
+            left+=1;
+            while(right >= left && s.charAt(right) == s.charAt(right-1)){
+                right-=1;
+            }
+            right-=1;
         }
-        return r - l + 1;
+        return right - left + 1;
     }
 }
