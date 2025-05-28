8e04e22111705850a1af5cd9dcf09da7d897aaf3
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-05-28 10:30:29 -0700

    Time: 13 ms (17.30%) | Memory: 69 MB (9.23%) - LeetSync

---
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.java
@@ -0,0 +1,67 @@
+class Solution {
+    public int maxDistance(List<List<Integer>> arrays) {
+        int [][] maxes = new int[2][2];
+        for(int [] arr : maxes){
+            Arrays.fill(arr,Integer.MIN_VALUE);
+        }
+        int [][] mins = new int[2][2];
+        for(int [] arr : mins){
+            Arrays.fill(arr,Integer.MAX_VALUE);
+        }
+        for(int i = 0;i<arrays.size();i++){
+            if(arrays.get(i).get(0) < mins[0][0]){
+                mins[1][0] = mins[0][0];
+                mins[1][1] = mins[0][1];
+                mins[0][0] = arrays.get(i).get(0);
+                mins[0][1] = i;
+            }
+            else if(arrays.get(i).get(0) < mins[1][0]){
+                mins[1][0] = arrays.get(i).get(0);
+                mins[1][1] = i;
+            }
+        }
+        for(int i = 0;i<arrays.size();i++){
+            if(arrays.get(i).get(arrays.get(i).size()-1) > maxes[0][0]){
+                maxes[1][0] = maxes[0][0];
+                maxes[1][1] = maxes[0][1];
+                maxes[0][0] = arrays.get(i).get(arrays.get(i).size()-1);
+                maxes[0][1] = i;
+            }
+            else if(arrays.get(i).get(arrays.get(i).size()-1) > maxes[1][0]){
+                maxes[1][0] = arrays.get(i).get(arrays.get(i).size()-1);
+                maxes[1][1] = i;
+            }
+        }
+        // System.out.println(mins[0][0] + "," + mins[0][1]);
+        // System.out.println(mins[1][0] + "," + mins[1][1]);
+        // System.out.println(maxes[0][0] + "," + maxes[0][1]);
+        // System.out.println(maxes[1][0] + "," + maxes[1][1]);
+        int max = 0;
+        if(mins[0][1] != maxes[0][1]){
+            max = Math.max(max,maxes[0][0] - mins[0][0]);
+        }
+        if(mins[0][1] != maxes[1][1]){
+            max = Math.max(max,maxes[1][0] - mins[0][0]);
+        }
+        if(mins[1][1] != maxes[0][1]){
+            max = Math.max(max,maxes[0][0] - mins[1][0]);
+        }
+        if(mins[1][1] != maxes[1][1]){
+            max = Math.max(max,maxes[1][0] - mins[1][0]);
+        }
+
+        return max;
+
+
+        // int max = 0;
+        // for(int i = 0;i<arrays.size();i++){
+        //     int currMin = arrays.get(i).get(0);
+        //     for(int j = 0;j<arrays.size();j++){
+        //         if(j != i){
+        //             max = Math.max(max,arrays.get(j).get(arrays.get(j).size()-1) - currMin);
+        //         }
+        //     }
+        // }
+        // return max;
+    }
+}
