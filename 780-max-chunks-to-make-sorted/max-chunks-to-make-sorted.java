4a35e672068bf48ed1b8c01e93859f4f2cdcbb42
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-05-28 09:37:10 -0700

    Time: 0 ms (100.00%) | Memory: 41.4 MB (12.44%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
@@ -1,19 +1,11 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        int curr = 0;
-        int prev = -1;
+        int max = 0;
         int count = 0;
         for(int i = 0;i<arr.length;i++){
-            pq.add(arr[i]);
-            curr = i;
-            while(curr > prev && !pq.isEmpty() && pq.peek() == curr){
-                pq.poll();
-                curr-=1;
-            }
-            if(pq.isEmpty()){
-                prev = i;
-                count+=1;
+            max = Math.max(max,arr[i]);
+            if(max == i){
+                count +=1;
             }
         }
         return count;
