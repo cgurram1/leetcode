e7e06c8b3fa0c61307080e6fc4d8728abaa8dfb4
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-05-28 10:33:29 -0700

    Time: 6 ms (88.56%) | Memory: 64.4 MB (70.74%) - LeetSync

--- 624-maximum-distance-in-arrays/maximum-distance-in-arrays.java
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.java
@@ -9,26 +9,27 @@ class Solution {
             Arrays.fill(arr,Integer.MAX_VALUE);
         }
         for(int i = 0;i<arrays.size();i++){
-            if(arrays.get(i).get(0) < mins[0][0]){
+            List<Integer> list = arrays.get(i);
+            int first = list.get(0);
+            int last  = list.get(list.size()-1);
+            if(first < mins[0][0]){
                 mins[1][0] = mins[0][0];
                 mins[1][1] = mins[0][1];
-                mins[0][0] = arrays.get(i).get(0);
+                mins[0][0] = first;
                 mins[0][1] = i;
             }
-            else if(arrays.get(i).get(0) < mins[1][0]){
-                mins[1][0] = arrays.get(i).get(0);
+            else if(first < mins[1][0]){
+                mins[1][0] = first;
                 mins[1][1] = i;
             }
-        }
-        for(int i = 0;i<arrays.size();i++){
-            if(arrays.get(i).get(arrays.get(i).size()-1) > maxes[0][0]){
+            if(last > maxes[0][0]){
                 maxes[1][0] = maxes[0][0];
                 maxes[1][1] = maxes[0][1];
-                maxes[0][0] = arrays.get(i).get(arrays.get(i).size()-1);
+                maxes[0][0] = last;
                 maxes[0][1] = i;
             }
-            else if(arrays.get(i).get(arrays.get(i).size()-1) > maxes[1][0]){
-                maxes[1][0] = arrays.get(i).get(arrays.get(i).size()-1);
+            else if(last > maxes[1][0]){
+                maxes[1][0] = last;
                 maxes[1][1] = i;
             }
         }
@@ -45,7 +46,6 @@ class Solution {
         if(mins[1][1] != maxes[1][1]){
             max = Math.max(max,maxes[1][0] - mins[1][0]);
         }
-
         return max;
     }
 }
