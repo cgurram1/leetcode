3d78d90aa0558ef0762e00784418a8d49a462faa
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-07-05 08:31:46 -0700

    Time: 36 ms (28.54%) | Memory: 51 MB (67.71%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,31 +1,19 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int [] point : points){
-            pq.add(new Pair(point));
-            if(pq.size() > k){
-                pq.poll();
-            }
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a, b) -> Long.compare(
+                (long)b[0]*b[0] + (long)b[1]*b[1],
+                (long)a[0]*a[0] + (long)a[1]*a[1]
+            )
+        );
+        for (int[] p : points) {
+            pq.offer(p);
+            if (pq.size() > k) pq.poll();
         }
-        int [][] res = new int[k][2];
-        int i = 0;
-        while(!pq.isEmpty()){
-            res[i] = pq.poll().coordinate;
-            i++;
+        int[][] ans = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            ans[i] = pq.poll();
         }
-        return res;
+        return ans;
     }
-}
-class Pair implements Comparable<Pair>{
-    int [] coordinate;
-    double dist;
-    public Pair(int [] point){
-        coordinate = point;
-        dist = Math.sqrt(point[0]*point[0] + point[1]*point[1]);
-    }
-
-    public int compareTo(Pair other){
-        return this.dist - other.dist > 0 ? 1:-1;
-    }
-
 }
