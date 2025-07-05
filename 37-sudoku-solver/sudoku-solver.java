7c8e4bb7d7f48151ed9afe8d6d6f97d2ee25ddde
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-07-05 14:55:04 -0700

    Time: 411 ms (5.02%) | Memory: 45.2 MB (5.18%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,22 +1,25 @@
 class Solution {
     public void solveSudoku(char[][] board) {
-        Map<Integer,Set<Character>> rowMap = new HashMap<>();
-        Map<Integer,Set<Character>> colMap = new HashMap<>();
-        Map<Integer,Set<Character>> cellMap = new HashMap<>();
+        Set<Character>[] rowMap = new HashSet[9];
+        for (int i = 0; i < 9; i++) rowMap[i] = new HashSet<>();
+        Set<Character>[] colMap = new HashSet[9];
+        for (int i = 0; i < 9; i++) colMap[i] = new HashSet<>();
+        Set<Character>[] cellMap = new HashSet[9];
+        for (int i = 0; i < 9; i++) cellMap[i] = new HashSet<>();
         for(int i = 0;i<9;i++){
-            rowMap.put(i, new HashSet<>());
-            colMap.put(i, new HashSet<>());
-            cellMap.put(i, new HashSet<>());
+            rowMap[i] = new HashSet<>();
+            colMap[i] = new HashSet<>();
+            cellMap[i] = new HashSet<>();
         }
         int cell;
         List<int []> toBeFilled = new ArrayList<>();
         for(int i = 0;i<9;i++){
             for(int j = 0;j<9;j++){
                 if(board[i][j] != '.'){
-                    rowMap.get(i).add(board[i][j]);
-                    colMap.get(j).add(board[i][j]);
+                    rowMap[i].add(board[i][j]);
+                    colMap[j].add(board[i][j]);
                     cell = (i/3) + 3 * (j/3);
-                    cellMap.get(cell).add(board[i][j]);
+                    cellMap[cell].add(board[i][j]);
                 }
                 else{
                     toBeFilled.add(new int[]{i,j});
@@ -27,14 +30,14 @@ class Solution {
         return;
     }
 
-    public boolean canPlace(int i, int j, int cell, char ch, Map<Integer,Set<Character>> rowMap,Map<Integer,Set<Character>> colMap, Map<Integer,Set<Character>> cellMap){
-        if(rowMap.get(i).contains(ch) == true || colMap.get(j).contains(ch) == true || cellMap.get(cell).contains(ch) == true){
+    public boolean canPlace(int i, int j, int cell, char ch, Set<Character>[] rowMap,Set<Character>[] colMap, Set<Character>[] cellMap){
+        if(rowMap[i].contains(ch) == true || colMap[j].contains(ch) == true || cellMap[cell].contains(ch) == true){
             return false;
         }
         return true;
     }
 
-    public boolean fun(char[][] board,List<int []> toBeFilled, int curr,Map<Integer,Set<Character>> rowMap,Map<Integer,Set<Character>> colMap, Map<Integer,Set<Character>> cellMap){
+    public boolean fun(char[][] board,List<int []> toBeFilled, int curr,Set<Character>[] rowMap,Set<Character>[] colMap, Set<Character>[] cellMap){
         if(curr == toBeFilled.size()){
             return true;
         }
@@ -47,16 +50,16 @@ class Solution {
             ch = (char)(val + '0');
             if(canPlace(i,j,cell,ch,rowMap,colMap,cellMap) == true){
                 board[i][j] = ch;
-                rowMap.get(i).add(ch);
-                colMap.get(j).add(ch);
-                cellMap.get(cell).add(ch);
+                rowMap[i].add(ch);
+                colMap[j].add(ch);
+                cellMap[cell].add(ch);
                 if(fun(board,toBeFilled,curr+1,rowMap,colMap,cellMap) == true){
                     return true;
                 }
                 board[i][j] = '.';
-                rowMap.get(i).remove(ch);
-                colMap.get(j).remove(ch);
-                cellMap.get(cell).remove(ch);
+                rowMap[i].remove(ch);
+                colMap[j].remove(ch);
+                cellMap[cell].remove(ch);
             }
         }
         return false;
