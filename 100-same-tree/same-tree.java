3f40b35c737e1c8fca169e5c077ce58faa2210e5
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-07-04 18:43:05 -0700

    Time: 0 ms (100.00%) | Memory: 41.5 MB (19.68%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -18,15 +18,12 @@ class Solution {
         if(p == null && q == null){
             return true;
         }
-        else if(p == null){
+        if(p == null || q == null){
             return false;
         }
-        else if(q == null){
-            return false;
+        if(p.val == q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right)){
+            return true;
         }
-        if((p.val == q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right)) == false){
-            return false;
-        }
-        return true;
+        return false;
     }
 }
