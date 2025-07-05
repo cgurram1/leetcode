4cae9b4a0d816217468c519cfa503447da1b1805
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-07-05 08:57:42 -0700

    Time: 2 ms (100.00%) | Memory: 53.4 MB (44.64%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,31 +1,51 @@
-class Solution {
+public class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int [] point : points){
-            pq.add(new Pair(point));
-            if(pq.size() > k){
-                pq.poll();
+        quickselect(points, 0, points.length - 1, k);
+        // Return the first k points (now the k closest are anywhere in points[0..k-1])
+        return Arrays.copyOf(points, k);
+    }
+
+    private void quickselect(int[][] pts, int lo, int hi, int k) {
+        if (lo >= hi) return;
+
+        // pick pivot index as the middle via integer division
+        int midIndex = (lo + hi) / 2;
+        int pivotDist = distSq(pts[midIndex]);
+
+        int i = lo, j = hi;
+        while (i <= j) {
+            // move i forward while pts[i] is closer than pivot
+            while (distSq(pts[i]) < pivotDist) {
+                i++;
+            }
+            // move j backward while pts[j] is farther than pivot
+            while (distSq(pts[j]) > pivotDist) {
+                j--;
+            }
+            if (i <= j) {
+                swap(pts, i, j);
+                i++;
+                j--;
             }
         }
-        int [][] res = new int[k][2];
-        int i = 0;
-        while(!pq.isEmpty()){
-            res[i] = pq.poll().coordinate;
-            i++;
+
+        // Now all points in [lo..j] have dist ≤ pivot, and [i..hi] have dist ≥ pivot
+        if (k - 1 <= j) {
+            quickselect(pts, lo, j, k);       // target is in the left partition
+        } else if (k - 1 >= i) {
+            quickselect(pts, i, hi, k);       // target is in the right partition
         }
-        return res;
-    }
-}
-class Pair implements Comparable<Pair>{
-    int [] coordinate;
-    double dist;
-    public Pair(int [] point){
-        coordinate = point;
-        dist = point[0]*point[0] + point[1]*point[1];
+        // otherwise, we've partitioned such that exactly k points in [lo..hi]
     }
 
-    public int compareTo(Pair other){
-        return this.dist - other.dist > 0 ? 1:-1;
+    private int distSq(int[] p) {
+        // squared Euclidean distance from origin
+        return p[0]*p[0] + p[1]*p[1];
     }
 
+    private void swap(int[][] pts, int i, int j) {
+        int[] tmp = pts[i];
+        pts[i] = pts[j];
+        pts[j] = tmp;
+    }
 }
