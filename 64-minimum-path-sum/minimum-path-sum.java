adf8f52e783f907f5cd73dbf7aae5c92a3cacd78
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-07-12 09:20:17 -0700

    Time: 1 ms (99.67%) | Memory: 47.8 MB (31.95%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,24 +1,24 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int [][] dp = new int[grid.length+1][grid[0].length];
+        int [][] dp = new int[grid.length][grid[0].length];
         for(int [] arr : dp){
             Arrays.fill(arr,-1);
         }
         return fun(0,0,grid,dp);
     }
-    public int fun(int i,int j,int [][] grid, int [][] dp){
-        if(i == grid.length || j == grid[0].length){
+    public int fun(int x, int y, int [][] grid, int [][] dp){
+        if(x >= grid.length || y >= grid[0].length){
             return Integer.MAX_VALUE;
         }
-        if(i == grid.length-1 && j == grid[0].length-1){
-            return grid[i][j];
+        if(x == grid.length-1 && y == grid[0].length-1){
+            return grid[x][y];
         }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        int down = fun(i+1,j,grid,dp);
-        int right = fun(i,j+1,grid,dp);
-        dp[i][j] = Math.min(down,right) + grid[i][j];
-        return dp[i][j];
+        if(dp[x][y]!= -1){
+            return dp[x][y];
+        } 
+        int down = fun(x,y+1,grid,dp);
+        int right = fun(x+1,y,grid,dp);
+        dp[x][y] = Math.min(down, right) + grid[x][y];
+        return dp[x][y];
     }
 }
