64e230e897a5afbb6bd9480aa8cbba570c84e9dd
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-07-12 10:47:09 -0700

    Time: 6 ms (5.09%) | Memory: 42.5 MB (7.80%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,22 +1,26 @@
 class Solution {
     public int numDecodings(String s) {
-        // int[] dp = new int[s.length()+1];
-        // dp[s.length()] = 1;
-        int next_first=1, next_second=0;
-        for(int index = s.length()-1;index>=0;index--){
-            int one = 0,two = 0;
-            if(Integer.valueOf(s.substring(index, index + 1)) > 0 && Integer.valueOf(s.substring(index, index + 1)) < 10){
-                one = next_first;
-            }
-            if(index + 1 < s.length()){
-                if(Integer.valueOf(s.substring(index, index + 1)) != 0 && Integer.valueOf(s.charAt(index) + "" + s.charAt(index + 1)) <= 26){
-                    two = next_second;
-                }
-            }
-            // dp[index] = one + two;
-            next_second=next_first;
-            next_first=one+two;
+        int[] dp = new int[s.length()+1];
+        Arrays.fill(dp,-1);
+        return rec(s,0,dp);
+    }
+    public static int rec(String next,int index,int [] dp){
+        if(index == next.length()){
+            return 1;
         }
-        return next_first;
+        if(dp[index] != -1){
+            return dp[index];
+        }
+        int one = 0;
+        int two = 0;
+        if(Integer.valueOf(next.substring(index, index + 1)) > 0){
+            one = rec(next,index+1,dp);
+        }
+        if(index + 1 < next.length()){
+            if(Integer.valueOf(next.substring(index, index + 1)) != 0 && Integer.valueOf(next.charAt(index) + "" + next.charAt(index + 1)) <= 26){
+                two = rec(next,index+2,dp);
+            }
+        }
+        return dp[index] = one + two;
     }
 }
