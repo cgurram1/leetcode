dbdef53479a207b2627950d5b5e25191ac4983e8
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-07-11 23:21:26 -0700

    Time: 6 ms (72.34%) | Memory: 44.5 MB (82.67%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -2,14 +2,14 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
         int left = 0;
         int right = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
         int res = 0;
-        Map<Character, Integer> map = new HashMap<>();
         while(right < s.length()){
             if(map.containsKey(s.charAt(right)) && map.get(s.charAt(right)) >= left){
-                left = map.get(s.charAt(right))+1;
+                left = map.get(s.charAt(right)) + 1;
             }
             map.put(s.charAt(right),right);
-            res = Math.max(res,right-left + 1);
+            res = Math.max(res, right - left + 1);
             right+=1;
         }
         return res;
