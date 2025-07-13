f1a1f419d27cd0263dc9b5fb76eabdcc70352814
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-07-13 14:49:44 -0700

    Time: 5 ms (75.30%) | Memory: 58 MB (54.92%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,15 +1,16 @@
 class Solution {
     public int maxArea(int[] height) {
-        int l = 0;
-        int r =  height.length-1;
+        int left = 0;
+        int right = height.length-1;
         int res = 0;
-        while(l < r){
-            res = Math.max(res,Math.min(height[l],height[r]) * (r - l));
-            if(height[l] < height[r]){
-                l+=1;
+        while(left < right){
+            int currCap = Math.min(height[left],height[right]) * (right - left);
+            res = Math.max(currCap,res);
+            if(height[left] < height[right]){
+                left+=1;
             }
             else{
-                r-=1;
+                right-=1;
             }
         }
         return res;
