3d9f177d9755b4f90277d43a81acf8838d97514f
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-07-14 04:41:24 -0700

    Time: 17 ms (5.58%) | Memory: 43.4 MB (5.21%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int myAtoi(String s) {
+        boolean isNegative = false;
+        boolean isFirstDigitFound = false;
+        boolean isSignFound = false;
+        double num = 0;
+        for(int i = 0;i<s.length();i++){
+            if(s.charAt(i) == '-' && isFirstDigitFound == false && isSignFound == false){
+                isNegative = true;
+                isSignFound = true;
+            }
+            else if((s.charAt(i) == ' ' && isFirstDigitFound == false && isSignFound == false) || (s.charAt(i) == '+' && isFirstDigitFound == false && isSignFound == false)){
+                if(s.charAt(i) == '+'){
+                    isSignFound = true;
+                }
+            }
+            else if(s.charAt(i) >= '0' && s.charAt(i) <= '9'){
+                isFirstDigitFound = true;
+                num = num*10 + (s.charAt(i) - '0');
+                System.out.println(num);
+            }
+            else{
+                break;
+            }
+        }
+        if((int)num != num){
+            if(isNegative){
+                return Integer.MIN_VALUE;
+            }
+            else{
+                return Integer.MAX_VALUE;
+            }
+        }
+        if(isNegative){
+            return -(int)num;
+        }
+        return (int)num;
+    }
+}
