2e5ece2eb6026bbc2d146b342a2a26f17b7df2f6
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-07-21 14:25:05 -0700

    Time: 23 ms (11.41%) | Memory: 48.8 MB (56.89%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,28 +1,24 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int [][] dp = new int[coins.length+1][amount+1];
+        int [][] dp = new int[coins.length + 1][amount + 1];
         for(int [] arr : dp){
             Arrays.fill(arr,-1);
         }
-        return fun(0,0,amount, coins,dp);
+        return fun(0,amount, coins, dp);
     }
-    public int fun(int index, int cost, int amount, int [] coins,int [][] dp){
-        if(index == coins.length){
-            return 0;
-        }
-        if(cost == amount){
+    public int fun(int index, int rem, int [] coins, int [][] dp){
+        if(rem == 0){
             return 1;
         }
-        if(dp[index][cost] != -1){
-            return dp[index][cost];
+        if(index >= coins.length || rem < 0){
+            return 0;
         }
-        int pick = 0;
-        if(cost + coins[index] <= amount){
-            pick = fun(index, cost + coins[index], amount, coins,dp);
+        if(dp[index][rem] != -1){
+            return dp[index][rem];
         }
-        int notPick = fun(index + 1, cost,amount, coins,dp);
-        dp[index][cost] = pick + notPick;
-        return dp[index][cost];
+        int pick = fun(index, rem - coins[index], coins,dp);
+        int notPick = fun(index + 1, rem, coins,dp);
+        dp[index][rem] = pick + notPick;
+        return dp[index][rem];
     }
-
 }
