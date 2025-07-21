c958e3e5e17d4b1585f8c652f9f61a1138c8c984
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-07-21 15:13:39 -0700

    Time: 22 ms (55.17%) | Memory: 45.8 MB (64.37%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int [] freqs = new int[26];
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> {return b.freq - a.freq;});
+        for(int i = 0;i<tasks.length;i++){
+            freqs[tasks[i] - 'A']+=1;
+        }
+        List<Pair> list = new ArrayList<>();
+        for(int i = 0;i<26;i++){
+            if(freqs[i] > 0){
+                list.add(new Pair((char)(i + 'A'),freqs[i]));
+            }
+        }
+        int curr;
+        int res = 0;
+        while(list.size() > 0 || pq.size() > 0){
+            curr = n;
+            for(int i = 0;i<list.size();i++){
+                pq.add(list.get(i));
+            }
+            list.clear();
+            while(curr >= 0){
+                if(!pq.isEmpty()){
+                    Pair p = pq.poll();
+                    p.freq -=1;
+                    if(p.freq > 0){
+                        list.add(p);
+                    }
+                }
+                if(list.size() == 0 && pq.size() == 0){
+                    return res+1;
+                }
+                res+=1;
+                curr-=1;
+            }
+        }
+        return res;
+    }
+}
+class Pair{
+    char ch;
+    int freq;
+    public Pair(char c, int f){
+        this.ch = c;
+        this.freq = f;
+    }
+}
