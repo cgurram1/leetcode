faf28c3f9d01e1a3e2a920b70678619b90b25ddb
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-08-11 22:13:53 -0700

    Time: 7 ms (44.63%) | Memory: 45.3 MB (60.69%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,33 +1,32 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adjList = new ArrayList<>();
+        List<List<Integer>> graph = new ArrayList<>();
         for(int i = 0;i<numCourses;i++){
-            adjList.add(new ArrayList<>());
+            graph.add(new ArrayList<>());
         }
+        int count = 0;
         int [] inDegrees = new int[numCourses];
         for(int i = 0;i<prerequisites.length;i++){
-            adjList.get(prerequisites[i][1]).add(prerequisites[i][0]);
+            graph.get(prerequisites[i][1]).add(prerequisites[i][0]);
             inDegrees[prerequisites[i][0]]+=1;
         }
-        int coursesFinished = 0;
-        Queue<Integer> queue = new LinkedList<>();
-        for(int i = 0;i<numCourses;i++){
+        Stack<Integer> stack = new Stack();
+        for(int i = 0;i<inDegrees.length;i++){
             if(inDegrees[i] == 0){
-                queue.add(i);
-                coursesFinished+=1;
+                stack.push(i);
             }
         }
-        while(!queue.isEmpty()){
-            int curr = queue.poll();
-            for(int i = 0;i<adjList.get(curr).size();i++){
-                inDegrees[adjList.get(curr).get(i)]-=1;
-                if(inDegrees[adjList.get(curr).get(i)] == 0){
-                    queue.add(adjList.get(curr).get(i));
-                    coursesFinished+=1;
+        while(!stack.isEmpty()){
+            int currentCourse = stack.pop();
+            count+=1;
+            for(int nextCourse : graph.get(currentCourse)){
+                inDegrees[nextCourse]-=1;
+                if(inDegrees[nextCourse] == 0){
+                    stack.push(nextCourse);
                 }
             }
         }
-        if(coursesFinished == numCourses){
+        if(count == numCourses){
             return true;
         }
         return false;
