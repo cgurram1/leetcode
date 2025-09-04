38c2ca1a04ad4106ca3ebce62c2d649bdd200552
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-09-04 16:22:06 -0700

    Time: 3 ms (45.15%) | Memory: 45 MB (38.43%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,69 +1,41 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
-    private List<List<String>> res;
-
+    List<List<String>> resultG = new ArrayList<>();
+    boolean [] colCheck;
+    int N;
     public List<List<String>> solveNQueens(int n) {
-        res = new ArrayList<>();
-        char[][] board = new char[n][n];
+        List<String> curr = new ArrayList<>();
+        colCheck = new boolean[n];
+        N = n;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                board[i][j] = '.';
-            }
+            curr.add(".".repeat(n));
         }
-        fun(board, n);
-        return res;
+        fun(0,curr);
+        return resultG;
     }
-    public boolean fun(char[][] board, int remaining) {
-        int row = board.length - remaining;
-        if (remaining == 0) {
-            List<String> list = new ArrayList<>();
-            for (char[] r : board) {
-                list.add(new String(r));
-            }
-            res.add(list);
-            return false;
+    public void fun(int queens, List<String> curr){
+        if(queens == N){
+            resultG.add(new ArrayList<>(curr));
+            return;
         }
-        int size = board.length;
-        for (int j = 0; j < size; j++) {
-            if (canPlace(board, row, j)) {
-                board[row][j] = 'Q';
-                fun(board, remaining - 1);
-                board[row][j] = '.';
+        for(int j = 0;j<N;j++){
+            if (!colCheck[j] && diagCheck(queens, j, curr)) {
+                colCheck[j] = true;
+                char[] row = curr.get(queens).toCharArray();
+                row[j] = 'Q';
+                curr.set(queens, new String(row));
+                fun(queens + 1, curr);
+                row[j] = '.';
+                curr.set(queens, new String(row));
+                colCheck[j] = false;
             }
         }
-        return false;
     }
-    public boolean canPlace(char[][] board, int x, int y) {
-        int n = board.length;
-        // check column and row
-        for (int k = 0; k < n; k++) {
-            if (board[k][y] == 'Q' || board[x][k] == 'Q') {
-                return false;
-            }
+    public boolean diagCheck(int r, int c, List<String> curr) {
+        for (int i = r - 1, j = c - 1; i >= 0 && j >= 0; i--, j--) {
+            if (curr.get(i).charAt(j) == 'Q') return false;
         }
-        // check four diagonals
-        int tempx, tempy;
-        tempx = x; tempy = y;
-        while (tempx < n && tempy < n) {
-            if (board[tempx][tempy] == 'Q') return false;
-            tempx++; tempy++;
-        }
-        tempx = x; tempy = y;
-        while (tempx >= 0 && tempy >= 0) {
-            if (board[tempx][tempy] == 'Q') return false;
-            tempx--; tempy--;
-        }
-        tempx = x; tempy = y;
-        while (tempx < n && tempy >= 0) {
-            if (board[tempx][tempy] == 'Q') return false;
-            tempx++; tempy--;
-        }
-        tempx = x; tempy = y;
-        while (tempx >= 0 && tempy < n) {
-            if (board[tempx][tempy] == 'Q') return false;
-            tempx--; tempy++;
+        for (int i = r - 1, j = c + 1; i >= 0 && j < N; i--, j++) {
+            if (curr.get(i).charAt(j) == 'Q') return false;
         }
         return true;
     }
