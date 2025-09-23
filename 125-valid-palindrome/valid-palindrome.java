dddfc6fbc042b34062800528a5f1ed8a2ed6e6b9
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-09-23 14:32:53 -0700

    Time: 5 ms (47.20%) | Memory: 49.2 MB (5.28%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,26 +1,20 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int left = 0, right = s.length() - 1;
-
-        while (left < right) {
-            // Move left pointer until it finds an alphanumeric character
-            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
-                left++;
-            }
-
-            // Move right pointer until it finds an alphanumeric character
-            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
-                right--;
-            }
-
-            // Compare characters (ignore case)
-            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
-                return false;
-            }
-
-            left++;
-            right--;
+        s = s.toLowerCase();
+        StringBuilder sb = new StringBuilder(s.length());
+        for(int i = 0;i<s.length();i++){
+            if(s.charAt(i) >= 'a' && s.charAt(i) <= 'z' || s.charAt(i) >= '0' && s.charAt(i) <= '9')
+            sb.append(s.charAt(i));
         }
-        return true;
+        return fun(sb.toString(),0);
+    }
+    public boolean fun(String s,int i){
+        if(i >= s.length()/2){
+            return true;
+        }
+        if(s.charAt(i) == s.charAt(s.length()-1-i) && fun(s,i+1)){
+            return true;
+        }
+        return false;
     }
 }
