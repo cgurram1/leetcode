58d641a441d468e5960e28926fbe3231c4233b9a
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-09-29 22:32:15 -0700

    Time: 0 ms (100.00%) | Memory: 43.4 MB (5.45%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,12 +1,13 @@
 class Solution {
-    List<List<Integer>> result = new ArrayList<>();
-    int [] numsG;
+    public List<List<Integer>> result;
+    public int [] numsG;
     public List<List<Integer>> subsets(int[] nums) {
+        result = new ArrayList<>();
         numsG = nums;
         fun(0,new ArrayList<>());
         return result;
     }
-    public void fun(int index,List<Integer> curr){
+    public void fun(int index, List<Integer> curr){
         if(index == numsG.length){
             result.add(new ArrayList<>(curr));
             return;
