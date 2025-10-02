cd14679ca76b533e085368dd4205ed06a840dd3a
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-10-01 17:09:00 -0700

    Time: 3 ms (43.59%) | Memory: 45.6 MB (58.22%) - LeetSync

---
+++ 800-letter-case-permutation/letter-case-permutation.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<String> result;
+    public String S;
+    public List<String> letterCasePermutation(String s) {
+        result = new ArrayList<>();
+        S = s.toLowerCase();
+        fun(0,new StringBuilder());
+        return result;
+    }
+    public void fun(int index, StringBuilder str){
+        if(index == S.length()){
+            result.add(new String(str.toString()));
+            return;
+        }
+        if(Character.isDigit(S.charAt(index))){
+            str.append(S.charAt(index));
+            fun(index+1,str);
+            str.setLength(str.length()-1);
+        }
+        else{
+            str.append(S.charAt(index));
+            fun(index+1,str);
+            str.setLength(str.length()-1);
+            str.append((S.charAt(index)+"").toUpperCase());
+            fun(index+1,str);
+            str.setLength(str.length()-1);
+        }
+    }
+}
