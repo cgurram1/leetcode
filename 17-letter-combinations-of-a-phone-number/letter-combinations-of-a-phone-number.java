29d581a96135c1027c2edf4b79515490768b2fb9
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-10-07 23:39:21 -0700

    Time: 0 ms (100.00%) | Memory: 42.5 MB (27.00%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,31 +1,34 @@
 class Solution {
+    public List<String> result;
+    public String digitsG;
+    public Map<Character, char []> phone;
     public List<String> letterCombinations(String digits) {
-        List<String> result = new ArrayList<>();
-        if(digits.length() == 0){
-            return result;
-        }
-        Map<Character, char[]> phone = new HashMap<>();
-        phone.put('2', new char[]{'a', 'b', 'c'});
-        phone.put('3', new char[]{'d', 'e', 'f'});
-        phone.put('4', new char[]{'g', 'h', 'i'});
-        phone.put('5', new char[]{'j', 'k', 'l'});
-        phone.put('6', new char[]{'m', 'n', 'o'});
-        phone.put('7', new char[]{'p', 'q', 'r', 's'});
-        phone.put('8', new char[]{'t', 'u', 'v'});
-        phone.put('9', new char[]{'w', 'x', 'y', 'z'});
-        StringBuilder curr = new StringBuilder();
-        rec(0,phone,curr,result,digits);
+        result = new ArrayList<>();
+        digitsG = digits;
+        phone = new HashMap<>();
+        phone.put('2', new char []{'a','b','c'});
+        phone.put('3', new char []{'d','e','f'});
+        phone.put('4', new char []{'g','h','i'});
+        phone.put('5', new char []{'j','k','l'});
+        phone.put('6', new char []{'m','n','o'});
+        phone.put('7', new char []{'p','q','r','s'});
+        phone.put('8', new char []{'t','u','v'});
+        phone.put('9', new char []{'w','x','y','z'});
+        fun(0,new StringBuilder());
         return result;
     }
-    public void rec(int index, Map<Character, char[]> phone,StringBuilder curr, List<String> result, String digits){
-        if(index == digits.length()){
-            result.add(new StringBuilder(curr).toString());
+    public void fun(int index, StringBuilder str){
+        if(index == digitsG.length()){
+            if(str.length() > 0){
+                result.add(str.toString());
+            }
             return;
         }
-        for(char next : phone.get(digits.charAt(index))){
-            curr.append(next);
-            rec(index+1,phone,curr, result, digits);
-            curr.deleteCharAt(curr.length() - 1);
+        for(int i = 0;i<phone.get(digitsG.charAt(index)).length;i++){
+            str.append(phone.get(digitsG.charAt(index))[i]);
+            fun(index + 1, str);
+            str.setLength(str.length() - 1);
         }
+
     }
 }
