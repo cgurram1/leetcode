baf3f1c8e73133736b149f7e8bdd026be4dc4c8a
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-11-14 17:39:36 -0800

    Time: 41 ms (55.26%) | Memory: 46.1 MB (33.62%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,58 +1,18 @@
-import java.util.Arrays;
-
 class Solution {
-    // public int[] nums;
-    // public int[][] dp;
-
-    // public int lengthOfLIS(int[] nums) {
-    //     // this.nums = nums;
-    //     // int n = nums.length;
-    //     // dp = new int[n][n + 1];
-    //     // for (int[] arr : dp) {
-    //     //     Arrays.fill(arr, -1);
-    //     // }
-    //     // return fun(0, -1);
-    // }
-
-    // public int fun(int index, int prevIndex) {
-    //     if (index == nums.length) return 0;
-    //     if (dp[index][prevIndex + 1] != -1) {
-    //         return dp[index][prevIndex + 1];
-    //     }
-    //     int pick = 0;
-    //     if (prevIndex == -1 || nums[index] > nums[prevIndex]) {
-    //         pick = 1 + fun(index + 1, index);
-    //     }
-    //     int notPick = fun(index + 1, prevIndex);
-    //     dp[index][prevIndex + 1] = Math.max(pick, notPick);
-    //     return dp[index][prevIndex + 1];
-    // }
-
     public int lengthOfLIS(int[] nums) {
-        List<Integer> temp = new ArrayList<>();
+        int [] vals = new int[nums.length];
+        int res = 0;
         for(int i = 0;i<nums.length;i++){
-            if(temp.size() == 0 || nums[i] > temp.get(temp.size()-1)){
-                temp.add(nums[i]);
+            int max = 0;
+            for(int j = 0;j<i;j++){
+                if(nums[j] < nums[i]){
+                    max = Math.max(max,vals[j]);
+                }
             }
-            else{
-                BinaryInsert(temp,nums[i]);
-            }
+            vals[i] = max+1;
+            res = Math.max(vals[i],res);
         }
-        return temp.size();
-    }
-    public void BinaryInsert(List<Integer> temp, int num){
-        int left = 0;
-        int right = temp.size()-1;
-        while(left < right){
-            int mid = (left + right)/2;
-            if(num > temp.get(mid)){
-                left = mid + 1;
-            }
-            else{
-                right = mid;
-            }
-        }
-        temp.set(right,num);
-    }
+        return res;
 
+    }
 }
