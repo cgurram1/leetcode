40f3fd9e7a4044092237433864b090a675aafc26
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-12-13 22:06:49 -0800

    Time: 3 ms (20.83%) | Memory: 47.4 MB (9.46%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,36 +1,23 @@
 class Solution {
     public int jump(int[] nums) {
-        // int[] dp = new int[nums.length];
-        // Arrays.fill(dp, -1);
-        // return dfs(0, nums, dp);
-        int left = 0;
-        int right = 0;
-        int jumps = 0;
-        while(right < nums.length-1){
-            int max = Integer.MIN_VALUE;
-            for(int i = left;i<=right;i++){
-                max = Math.max(max,i + nums[i]);
-            }
-            left = right + 1;
-            right = max;
-            jumps+=1;
+        // [2,3,1,1,4]
+        //    ...
+        if(nums.length <= 1){
+            return 0;
         }
-        return jumps;
+        int steps = 1;
+        int [] currRange = new int[]{1,nums[0]};
+        while(currRange[1] < nums.length-1){
+            steps+=1;
+            int maxR = 0;
+            for(int i = currRange[0];i <= currRange[1];i++){
+                maxR = Math.max(maxR, i + nums[i]);
+            }
+            // if(maxR > currRange[1]){
+                currRange[0] +=1;
+                currRange[1] = maxR;
+            // }
+        }
+        return steps;
     }
-
-
-    // public int dfs(int index, int[] nums, int[] dp) {
-    //     if (index >= nums.length - 1) return 0;
-    //     if (dp[index] != -1) return dp[index];
-
-    //     int min = Integer.MAX_VALUE;
-    //     for (int i = 1; i <= nums[index]; i++) {
-    //         int next = dfs(index + i, nums, dp);
-    //         if (next != Integer.MAX_VALUE) {
-    //             min = Math.min(min, 1 + next);
-    //         }
-    //     }
-
-    //     return dp[index] = min;
-    // }
 }
