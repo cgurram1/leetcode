a268224dcc34b54c4ee2bb85f9f56f5ec2ff680a
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-12-25 18:24:40 -0800

    Time: 51 ms (5.00%) | Memory: 74.2 MB (5.52%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
@@ -18,21 +18,17 @@ class Solution {
         int [][] right = new int[customers.length()+1][2];
         for(int i = 1;i<=customers.length();i++){
             if(customers.charAt(i-1) == 'Y'){
-                left[i][0] = left[i-1][0] + 1;
                 left[i][1] = left[i-1][1];
             }
             else{
-                left[i][0] = left[i-1][0];
                 left[i][1] = left[i-1][1] + 1;
             }
         }
         for(int i = customers.length()-2;i>=-1;i--){
             if(customers.charAt(i+1) == 'Y'){
                 right[i+1][0] = right[i+2][0] + 1;
-                right[i+1][1] = right[i+2][1];
             }
             else{
-                right[i+1][1] = right[i+2][1] + 1;
                 right[i+1][0] = right[i+2][0];
             }
         }
@@ -44,10 +40,6 @@ class Solution {
                 res = i;
             }
         }
-        // for(int i = 0;i<left.length;i++){
-        //     System.out.print("Y : " + left[i][0] + " | N : " + left[i][1] + " ||| ");
-        //     System.out.println("Y : " + right[i][0] + " | N : " + right[i][1]);
-        // }
         return res;
     }
 }
