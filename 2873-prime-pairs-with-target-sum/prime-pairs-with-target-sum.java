f52c715045374bb3c52ad521a05720ba784964b4
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2025-12-29 15:30:28 -0800

    Time: 221 ms (29.59%) | Memory: 88.7 MB (9.18%) - LeetSync

--- 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.java
+++ 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.java
@@ -1,39 +1,31 @@
+/**
+
+[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+[0,1,2,,4,,6,,8,9,10,,12,,14,15,16,,18,19]
+
+ */
 class Solution {
     public List<List<Integer>> findPrimePairs(int n) {
-        // Set<Integer> primes = new HashSet<>();
-        // PriorityQueue<List<Integer>> pq = new PriorityQueue<>((a,b)->{
-        //     return a.get(0) - b.get(0);
-        // });
-        // int flag = 0;
+        int [] primes = new int[n+1];
+        Arrays.fill(primes,-1);
+        for(int i = 2;i<=n;i++){
+            if(primes[i] == -1){
+                int mul = 2;
+                while(i * mul <= n){
+                    primes[i*mul] = i*mul;
+                    mul+=1;
+                }
+            }
+        }
         List<List<Integer>> result = new ArrayList<>();
         for(int i = 2;i<=n/2;i++){
-            if(isPrime(i) && isPrime(n-i)){
+            if(primes[i] == -1 && primes[n-i] == -1){
                 List<Integer> temp = new ArrayList<>();
                 temp.add(i);
                 temp.add(n-i);
                 result.add(temp);
             }
         }
-        // while(pq.isEmpty() == false){
-        //     result.add(pq.poll());
-        // }
-        // if(flag == 1){
-        //     List<Integer> temp = new ArrayList<>();
-        //     temp.add((int)(n/2));
-        //     temp.add((int)(n/2));
-        //     result.add(temp);
-        // }
         return result;
-    }
-    public boolean isPrime(int n){
-        if(n == 2 || n == 3){
-            return true;
-        }
-        for(int i = 2;i*i<=n;i++){
-            if(n % i == 0){
-                return false;
-            }
-        }
-        return true;
     }
 }
