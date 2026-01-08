b47bac0d001e33ae0b4afb47996ffad1d33b9a04
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-01-08 10:58:13 -0800

    Time: 486 ms (8.54%) | Memory: 111.8 MB (5.53%) - LeetSync

---
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.py
@@ -0,0 +1,21 @@
+"""
+ l 
+[2,1,-2,5]
+
+ r
+[3,0,-6]
+
+"""
+
+class Solution:
+    def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
+        @lru_cache(None)
+        def rec(left,right):
+            if(left == len(nums1) or right == len(nums2)):
+                return float('-inf')
+            both = nums1[left] * nums2[right] + max(0,rec(left + 1, right + 1))
+            top = rec(left+1,right)
+            bottom = rec(left,right + 1)
+            none = rec(left +1,right + 1)
+            return max(both, top, bottom, none)
+        return rec(0,0)
