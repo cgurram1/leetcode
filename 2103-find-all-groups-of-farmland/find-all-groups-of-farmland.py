e26a8c2ac224a41a4d79012702d3e12d9e705f24
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-01-08 13:00:00 -0800

    Time: 307 ms (25.67%) | Memory: 48.7 MB (12.65%) - LeetSync

--- 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.py
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.py
@@ -2,21 +2,20 @@ class Solution:
     def findFarmland(self, land: List[List[int]]) -> List[List[int]]:
         x = [1,-1,0,0]
         y = [0,0,1,-1]
-        visited = set()
         result = []
 
         def dfs(i,j):
-            nonlocal curr,visited
-            visited.add((i,j))
+            nonlocal curr
+            land[i][j] = 0
             curr[0] = max(curr[0],i)
             curr[1] = max(curr[1],j)
             for k in range(4):
-                if(i + x[k] >= 0 and i + x[k] < len(land) and j + y[k] >= 0 and j + y[k] < len(land[0]) and (i+x[k], j+y[k]) not in visited and land[i+x[k]][j+y[k]] == 1):
+                if(i + x[k] >= 0 and i + x[k] < len(land) and j + y[k] >= 0 and j + y[k] < len(land[0]) and land[i+x[k]][j+y[k]] == 1):
                     dfs(i+x[k],j+y[k])
         
         for i in range(len(land)):
             for j in range(len(land[0])):
-                if((i,j) not in visited and land[i][j] == 1):
+                if(land[i][j] == 1):
                     curr = [-1,-1]
                     dfs(i,j)
                     result.append([i,j] + curr.copy())
