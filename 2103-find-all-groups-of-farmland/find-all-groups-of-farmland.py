66e72d8ebac3e5a759524a5074d0a38a0c410224
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-01-08 13:02:38 -0800

    Time: 199 ms (54.02%) | Memory: 49 MB (12.65%) - LeetSync

--- 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.py
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.py
@@ -2,14 +2,14 @@ class Solution:
     def findFarmland(self, land: List[List[int]]) -> List[List[int]]:
         rows, cols = len(land), len(land[0])
         result = []
-
+        dirs = ((1,0), (-1,0), (0,1), (0,-1))
         def dfs(i, j):
             nonlocal curr
             land[i][j] = 0
             curr[0] = max(curr[0], i)
             curr[1] = max(curr[1], j)
 
-            for di, dj in ((1,0), (-1,0), (0,1), (0,-1)):
+            for di, dj in dirs:
                 ni, nj = i + di, j + dj
                 if 0 <= ni < rows and 0 <= nj < cols and land[ni][nj] == 1:
                     dfs(ni, nj)
@@ -17,7 +17,7 @@ class Solution:
         for i in range(rows):
             for j in range(cols):
                 if land[i][j] == 1:
-                    curr = [-1,-1]
+                    curr = [i,j]
                     dfs(i, j)
                     result.append([i,j] + curr.copy())
 
