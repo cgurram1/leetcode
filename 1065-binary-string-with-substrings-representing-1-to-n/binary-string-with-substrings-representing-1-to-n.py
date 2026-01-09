d2ada5bb880384e50ba81eac033681dd533a4270
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-01-09 14:22:59 -0800

    Time: 23 ms (12.24%) | Memory: 21.9 MB (6.29%) - LeetSync

--- 1065-binary-string-with-substrings-representing-1-to-n/binary-string-with-substrings-representing-1-to-n.py
+++ 1065-binary-string-with-substrings-representing-1-to-n/binary-string-with-substrings-representing-1-to-n.py
@@ -1,14 +1,16 @@
 class Solution:
     def queryString(self, s: str, n: int) -> bool:
         nums = set()
-
+        strs = set()
         for i in range(len(s)):
             left = 0
             right = i+1
             while(right <= len(s) and len(nums) < n):
-                val = int(s[left:right],2)
-                if(0 < val <= n):
-                    nums.add(val)
+                if s[left:right] not in strs:
+                    strs.add(s[left:right])
+                    val = int(s[left:right],2)
+                    if(0 < val <= n):
+                        nums.add(val)
                 right+=1
                 left+=1
         # print(nums)
