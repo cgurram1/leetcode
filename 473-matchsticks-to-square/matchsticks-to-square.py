765cd30b40f2380dba92ed5ec118720911ca3687
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-02-22 21:04:20 -0800

    Time: 188 ms (72.31%) | Memory: 19.7 MB (18.00%) - LeetSync

--- 473-matchsticks-to-square/matchsticks-to-square.py
+++ 473-matchsticks-to-square/matchsticks-to-square.py
@@ -1,21 +1,25 @@
 class Solution:
     def makesquare(self, matchsticks: List[int]) -> bool:
-        matchsticks.sort(reverse=True)
-        s = sum(matchsticks)
-        if (float(s // 4) != s/4) or matchsticks[0] > s//4:
+        total = sum(matchsticks)
+        groupSize = total//4
+        matchsticks = sorted(matchsticks,reverse=True)
+        print(matchsticks)
+        if groupSize != (int)(total/4):
             return False
-        target = s // 4
-        arr = [0] * 4
-        def myFun(index):
-            if index == len(matchsticks):
+        if(max(matchsticks) > groupSize):
+            return False
+        def fun(index,groups):
+            if(index == len(matchsticks)):
                 return True
             for i in range(4):
-                if arr[i]+matchsticks[index] <= target:
-                    arr[i]+=matchsticks[index]
-                    if myFun(index+1):
+                if i > 0 and groups[i] == groups[i-1]:
+                    continue
+                if(groups[i] + matchsticks[index] <= groupSize):
+                    groups[i] += matchsticks[index]
+                    if fun(index + 1, groups):
                         return True
-                    arr[i]-=matchsticks[index]
-                    if arr[i] == 0:
-                        break
+                    groups[i] -= matchsticks[index]
             return False
-        return myFun(0)
+        return fun(0,[0,0,0,0])
+                
+
