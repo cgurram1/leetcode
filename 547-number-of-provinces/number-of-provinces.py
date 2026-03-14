654e95bb6aa23a9118233ac0fc7fb8af62d51ec6
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-03-14 14:13:43 -0700

    Time: 8 ms (30.75%) | Memory: 20.8 MB (43.84%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,14 +1,28 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        adjList = [[] for _ in range(len(isConnected))]
+        for r in range(len(isConnected)):
+            for c in range(len(isConnected)):
+                if r != c and isConnected[r][c] == 1:
+                    adjList[r].append(c)
+        def BFS(node):
+            queue = [node]
+            while queue:
+                currNode = queue.pop()
+                for adjNode in adjList[currNode]:
+                    if visited[adjNode] == 0:
+                        visited[adjNode] = 1
+                        queue.append(adjNode)
+        def DFS(node):
+            visited[node] = 1
+            for adjNode in adjList[node]:
+                if visited[adjNode] == 0:
+                    DFS(adjNode)
+        res = 0
         visited = [0 for _ in range(len(isConnected))]
-        count = 0
-        def dfs(i):
-            visited[i] = 1
-            for k in range(len(isConnected)):
-                if isConnected[i][k] == 1 and visited[k] == 0:
-                    dfs(k)
-        for i in range(len(isConnected)):
-            if visited[i] == 0:
-                count+=1
-                dfs(i)
-        return count
+        for node in range(len(isConnected)):
+            if visited[node] == 0:
+                res+=1
+                # BFS(node)
+                DFS(node)
+        return res
