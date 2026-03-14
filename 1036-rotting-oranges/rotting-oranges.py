498bd49e653b7ceb074148d9a038773b2e5bdf67
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-03-14 15:04:10 -0700

    Time: 3 ms (76.03%) | Memory: 19.3 MB (92.44%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,37 +1,28 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        levels = 0
+        visited = [[0] * len(grid[0]) for _ in range(len(grid))]
         queue = []
-        nextRotten = 0
-        res = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 2:
-                    queue.append((i,j))
-                    nextRotten+=1
-        x = [1,-1,0,0]
-        y = [0,0,1,-1]
-        while(queue):
-            print(queue,nextRotten)
-            rotten = nextRotten
-            nextRotten = 0
-            for _ in range(rotten):
-                i,j = queue.pop(0)
-                for k in range(4):
-                    nexti = i + x[k]
-                    nextj = j + y[k]
-                    if nexti >= 0 and nexti < len(grid) and nextj >= 0 and nextj < len(grid[0]) and grid[nexti][nextj] == 1:
-                        queue.append((nexti,nextj))
-                        grid[nexti][nextj] = 2
-                        nextRotten+=1
-            res+=1
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    print("Hello")
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 2:
+                    queue.append((r,c))
+        queue.append(None)
+        while queue[0] != None:
+            while queue[0] != None:
+                ci,cj = queue.pop(0)
+                for di,dj in [(1,0),(0,1),(-1,0),(0,-1)]:
+                    ni,nj = di + ci, dj + cj
+                    if ni >= 0 and ni < len(grid) and nj >= 0 and nj < len(grid[0]) and grid[ni][nj] == 1 and visited[ni][nj] == 0:
+                        visited[ni][nj] = 1
+                        queue.append((ni,nj))
+                        grid[ni][nj] = 2
+            queue.append(queue.pop(0))
+            levels+=1
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1:
                     return -1
-        if res == 0:
+        if levels <= 0:
             return 0
-        return res - 1
-        
-
-
+        return levels - 1
