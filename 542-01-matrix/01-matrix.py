e8e015ed946ad2fe9f4262c8ef4a30ba8d451e2f
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-03-14 21:09:52 -0700

    Time: 159 ms (27.39%) | Memory: 22.2 MB (63.21%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,20 +1,23 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         queue = deque()
-        result = [[float('inf')]* len(mat[0]) for _ in range(len(mat))]
-        for r in range(len(mat)):
-            for c in range(len(mat[0])):
+        n = len(mat)
+        m = len(mat[0])
+        result = [[float('inf')]* m for _ in range(n)]
+        for r in range(n):
+            for c in range(m):
                 if mat[r][c] == 0:
                     result[r][c] = 0
-                    queue.append((r,c,0))
+                    queue.append((r,c))
+        
         while queue:
-            cr,cc,level = queue.popleft()
+            cr,cc = queue.popleft()
             for di, dj in [(1,0),(0,1),(-1,0),(0,-1)]:
                 nr, nc = cr + di, cc + dj
-                if nr >= 0 and nr < len(mat) and nc >= 0 and nc < len(mat[0]):
+                if nr >= 0 and nr < n and nc >= 0 and nc < m:
                     if result[nr][nc] == float('inf'):
-                        result[nr][nc] = level + 1
-                        queue.append((nr,nc, level + 1))
+                        result[nr][nc] = result[cr][cc] + 1
+                        queue.append((nr,nc))
         return result
 
 
