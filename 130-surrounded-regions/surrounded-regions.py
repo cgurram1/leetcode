1a1c97d7182e2f8b94cce7a85e275222bb86d87a
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-03-14 21:33:15 -0700

    Time: 5 ms (64.37%) | Memory: 22.4 MB (73.11%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -3,33 +3,36 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        visited = [[0 for _ in range(len(board[0]))] for _ in range(len(board))]
-        x = [1,-1,0,0]
-        y = [0,0,-1,1]
-        def DFS(i,j):
-            visited[i][j] = 1
-            board[i][j] = "T"
-            for k in range(4):
-                nexti = i + x[k]
-                nextj = j + y[k]
-                if nexti >= 0 and nexti < len(board) and nextj >= 0 and nextj < len(board[0]) and visited[nexti][nextj] == 0 and board[nexti][nextj] == "O":
-                    DFS(nexti,nextj)
-        
-        for i in range(len(board)):
-            if board[i][0] == "O":
-                DFS(i, 0)
-            if board[i][-1] == "O":
-                DFS(i, len(board[0]) - 1)
-
-        for j in range(len(board[0])):
-            if board[0][j] == "O":
-                DFS(0, j)
-            if board[-1][j] == "O":
-                DFS(len(board) - 1, j)
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == "O":
-                    board[i][j] = "X"
-                elif board[i][j] == "T":
-                    board[i][j] = "O"
+        queue = deque()
+        m = len(board[0])
+        n = len(board)
+        for c in range(m):
+            if board[0][c] == 'O':
+                queue.append((0,c))
+            if board[n-1][c] == 'O':
+                queue.append((n-1,c))
+        for r in range(1,n-1):
+            if board[r][0] == 'O':
+                queue.append((r,0))
+            if board[r][m-1] == 'O':
+                queue.append((r,m-1))
+        print(queue)
+        def DFS(node):
+            x,y = node[0],node[1]
+            board[x][y] = -1
+            for di,dj in [(1,0),(0,1),(-1,0),(0,-1)]:
+                nr,nc = x + di, y + dj
+                if 0 <= nr < n and 0 <= nc < m and board[nr][nc] == 'O':
+                    board[nr][nc] = -1
+                    DFS((nr,nc))
+        while queue:
+            px,py = queue.popleft()
+            if board[px][py] == 'O':
+                DFS((px,py))
+        for r in range(n):
+            for c in range(m):
+                if board[r][c] == 'O':
+                    board[r][c] = 'X'
+                if board[r][c] == -1:
+                    board[r][c] = 'O'
 
