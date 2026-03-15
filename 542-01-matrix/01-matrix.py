4e7c8fec8f38cb9dd4751a615a0e361ed9ee99c8
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-03-14 21:07:10 -0700

    Time: 183 ms (18.84%) | Memory: 22.1 MB (68.18%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,24 +1,20 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        queue = []
+        queue = deque()
         result = [[float('inf')]* len(mat[0]) for _ in range(len(mat))]
         for r in range(len(mat)):
             for c in range(len(mat[0])):
                 if mat[r][c] == 0:
                     result[r][c] = 0
                     queue.append((r,c,0))
-        queue.append(None)
-        level = 0
-        while queue[0] != None:
-            while queue[0] != None:
-                cr,cc,level = queue.pop(0)
-                for di, dj in [(1,0),(0,1),(-1,0),(0,-1)]:
-                    nr, nc = cr + di, cc + dj
-                    if nr >= 0 and nr < len(mat) and nc >= 0 and nc < len(mat[0]):
-                        if result[nr][nc] == float('inf'):
-                            result[nr][nc] = level + 1
-                            queue.append((nr,nc, level + 1))
-            queue.append(queue.pop(0))
+        while queue:
+            cr,cc,level = queue.popleft()
+            for di, dj in [(1,0),(0,1),(-1,0),(0,-1)]:
+                nr, nc = cr + di, cc + dj
+                if nr >= 0 and nr < len(mat) and nc >= 0 and nc < len(mat[0]):
+                    if result[nr][nc] == float('inf'):
+                        result[nr][nc] = level + 1
+                        queue.append((nr,nc, level + 1))
         return result
 
 
