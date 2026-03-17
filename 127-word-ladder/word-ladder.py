17e608696cc7a9104792c8510d479597bc345ff7
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-03-17 16:27:56 -0700

    Time: 607 ms (16.63%) | Memory: 28.4 MB (5.03%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -6,22 +6,18 @@ class Solution:
                 pattern = word[:i] + '*' + word[i+1:]
                 patternMap[pattern].append(word)
         queue = deque()
-        queue.append(beginWord)
-        queue.append(None)
+        queue.append((beginWord,1))
         visited = set()
         level = 1
-        while queue[0] != None:
-            level += 1
-            while queue[0] != None:
-                currWord = queue.popleft()
-                visited.add(currWord)
-                for i in range(len(word)):
-                    pattern = currWord[:i] + '*' + currWord[i+1:]
-                    for adjWord in patternMap[pattern]:
-                        if adjWord == endWord:
-                            return level
-                        if adjWord not in visited:
-                            queue.append(adjWord)
-            queue.append(queue.popleft())
+        while queue:
+            currWord,level = queue.popleft()
+            visited.add(currWord)
+            for i in range(len(word)):
+                pattern = currWord[:i] + '*' + currWord[i+1:]
+                for adjWord in patternMap[pattern]:
+                    if adjWord == endWord:
+                        return level + 1
+                    if adjWord not in visited:
+                        queue.append((adjWord,level + 1))
         return 0
 
