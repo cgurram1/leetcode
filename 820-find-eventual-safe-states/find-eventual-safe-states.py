5b87ae59c170c89200b31fb800b9856ffc596ef8
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-03-17 10:44:33 -0700

    Time: 59 ms (35.03%) | Memory: 26 MB (68.11%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -1,22 +1,26 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        isSafe = [-1 for _ in range(len(graph))]
-        def dfs(Node):
-            if isSafe[Node] != -1:
-                if isSafe[Node] == 1:
-                    return True
-                else:
-                    return False
-            isSafe[Node] = 0
-            for nextNode in graph[Node]:
-                if dfs(nextNode) == False:
-                    return False
-            isSafe[Node] = 1
-            return True
-        res = []
-        for i in range(len(graph)):
-            if dfs(i) == True:
-                res.append(i)
-        return res
-            
+        reversedGraph = [[] for _ in range(len(graph))]
+        inDegrees = [0 for _ in range(len(graph))]
+        result = []
+        for node in range(len(graph)):
+            for nextNode in graph[node]:
+                reversedGraph[nextNode].append(node)
+                inDegrees[node]+=1
+        # print(graph)
+        # print(reversedGraph)
+        # print(inDegrees)
+        queue = deque()
+        for i,degree in enumerate(inDegrees):
+            if degree == 0:
+                queue.append(i)
+        # print(queue)
+        while queue:
+            currNode = queue.popleft()
+            result.append(currNode)
+            for adjNode in reversedGraph[currNode]:
+                inDegrees[adjNode]-=1
+                if inDegrees[adjNode] == 0:
+                    queue.append(adjNode)
+        return sorted(result)
 
