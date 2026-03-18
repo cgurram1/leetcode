914152759cf10ed0382c59d9a9fd0f34dfeeac16
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-03-18 12:27:48 -0700

    Time: 256 ms (61.35%) | Memory: 20.8 MB (84.45%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.py
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.py
@@ -1,25 +1,22 @@
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        res_min = float('inf')
-        row = len(heights)
-        col = len(heights[0])
-        if row == 1 and col == 1:
-            return 0
-        distance = [[float('inf') for _ in range(col)] for _ in range(row)]
-        x = [1,0,-1,0]
-        y = [0,-1,0,1]
+        pq = []
+        heapq.heappush(pq, (0,0,0))
+        dirs = [(1,0),(0,1),(-1,0),(0,-1)]
 
-        pq = [(0,(0,0))]
-        distance[0][0] = 0
-        while(pq):
-            diff,(i,j) = heapq.heappop(pq)
-            for k in range(4):
-                nexti = i + x[k]
-                nextj = j + y[k]
-                if nexti >= 0 and nexti < row and nextj >= 0 and nextj < col:
-                    effort = abs(heights[i][j] - heights[nexti][nextj])
-                    new_effort = max(diff,effort)
-                    if new_effort < distance[nexti][nextj]:
-                        distance[nexti][nextj] = new_effort
-                        heapq.heappush(pq,(new_effort,(nexti,nextj)))
-        return distance[row-1][col-1]
+        dists = [[float('inf')] * len(heights[0]) for _ in range(len(heights))]
+
+        dists[0][0] = 0
+        newEffort = float('-inf')
+        while pq:
+            currWeight, ci, cj = heapq.heappop(pq)
+            if currWeight > dists[ci][cj]:
+                continue
+            for di,dj in dirs:
+                ni,nj = ci + di, cj + dj
+                if 0 <= ni < len(heights) and 0 <= nj < len(heights[0]):
+                    newEffort = max(currWeight, abs(heights[ni][nj] - heights[ci][cj]))
+                    if dists[ni][nj] > newEffort:
+                        dists[ni][nj] = newEffort
+                        heapq.heappush(pq,(dists[ni][nj],ni,nj))
+        return dists[len(heights)-1][len(heights[0])-1]
