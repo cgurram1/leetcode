80f537f4a9d4f0f9756639d6880c910d3c04fbe6
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-03-18 20:58:57 -0700

    Time: 99 ms (86.31%) | Memory: 20.4 MB (45.89%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,24 +1,56 @@
 class Solution:
     def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
-        adjMatrix = [[float('inf') if i != j else 0 for i in range(n)] for j in range(n)]
-        for edge in edges:
-            adjMatrix[edge[0]][edge[1]] = edge[2]
-            adjMatrix[edge[1]][edge[0]] = edge[2]
-        for i in range(n):
-            for r in range(n):
-                for c in range(n):
-                    if adjMatrix[r][i] != float('inf') and adjMatrix[i][c] != float('inf'):
-                        adjMatrix[r][c] = min(adjMatrix[r][c], (adjMatrix[r][i] + adjMatrix[i][c]))
-        minCities = n
+        # adjMatrix = [[float('inf') if i != j else 0 for i in range(n)] for j in range(n)]
+        # for edge in edges:
+        #     adjMatrix[edge[0]][edge[1]] = edge[2]
+        #     adjMatrix[edge[1]][edge[0]] = edge[2]
+        # for i in range(n):
+        #     for r in range(n):
+        #         for c in range(n):
+        #             if adjMatrix[r][i] != float('inf') and adjMatrix[i][c] != float('inf'):
+        #                 adjMatrix[r][c] = min(adjMatrix[r][c], (adjMatrix[r][i] + adjMatrix[i][c]))
+        # minCities = n
+        # result = -1
+        # for i in range(n):
+        #     count = 0
+        #     for j in range(n):
+        #         if i != j and adjMatrix[i][j] <= distanceThreshold:
+        #             count+=1
+        #     if count <= minCities:
+        #         result = i
+        #         minCities = count
+        # return result
+        
+        def dijkshtra(src):
+            pq = []
+            heapq.heappush(pq, (0,src))
+            while pq:
+                currDist, currNode = heapq.heappop(pq)
+                if currDist > dists[currNode]:
+                    continue
+                for adjNode, weight in adjList[currNode]:
+                    if currDist + weight < dists[adjNode]:
+                        dists[adjNode] = currDist + weight
+                        heapq.heappush(pq, (dists[adjNode], adjNode))
+        
+        minCount = float('inf')
         result = -1
+        adjList = [[] for _ in range(n)]
+        for edge in edges:
+            adjList[edge[0]].append((edge[1],edge[2]))
+            adjList[edge[1]].append((edge[0],edge[2]))
+        
         for i in range(n):
+            dists = [float('inf') for _ in range(n)]
+            dists[i] = 0
+            dijkshtra(i)
             count = 0
             for j in range(n):
-                if i != j and adjMatrix[i][j] <= distanceThreshold:
+                if dists[j] <= distanceThreshold:
                     count+=1
-            if count <= minCities:
+            if count <= minCount:
                 result = i
-                minCities = count
+                minCount = count
         return result
 
                     
