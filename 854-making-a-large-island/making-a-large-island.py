46962dd69572c73042aa3c7f26a62cc375201506
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-03-20 11:46:36 -0700

    Time: 1669 ms (5.13%) | Memory: 103 MB (5.03%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.py
+++ 854-making-a-large-island/making-a-large-island.py
@@ -1,52 +1,59 @@
-"""
-1,1
-1,0
-
-
-"""
 class Solution:
-    def largestIsland(self, grid: List[List[int]]) -> int:
+    def largestIsland(self, grid):
+        n = len(grid)
         parent = {}
         size = {}
-        def find(cord):
-            if cord != parent[cord]:
-                parent[cord] = find(parent[cord])
-            return parent[cord]
-        def union(cord1, cord2):
-            p1 = find(cord1)
-            p2 = find(cord2)
+
+        def find(x):
+            if x != parent[x]:
+                parent[x] = find(parent[x])
+            return parent[x]
+
+        def union(x, y):
+            p1, p2 = find(x), find(y)
+            if p1 == p2:
+                return size[p1]
+
             if size[p1] < size[p2]:
                 p1, p2 = p2, p1
-            parent[p1] = p2
-            size[p2]+=size[p1]
-            size.pop(p1)
-            return size[p2]
-        lands = deque()
-        for i in range(len(grid)):
-            for j in range(len(grid)):
+
+            parent[p2] = p1
+            size[p1] += size[p2]
+
+            return size[p1]
+
+        # init
+        for i in range(n):
+            for j in range(n):
                 if grid[i][j] == 1:
                     parent[(i,j)] = (i,j)
                     size[(i,j)] = 1
-                    lands.append((i,j))
-        result = 1
-        while lands:
-            i,j = lands.popleft()
-            for di,dj in [(1,0),(0,1),(-1,0),(0,-1)]:
-                ni,nj = di + i, dj + j
-                if 0 <= ni < len(grid) and 0 <= nj < len(grid[0]) and grid[ni][nj] == 1 and find((i,j)) != find((ni,nj)):
-                    result = max(result,union((i,j),(ni,nj)))
-        
 
-        for i in range(len(grid)):
-            for j in range(len(grid)):
+        # union
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    for di, dj in [(1,0),(0,1),(-1,0),(0,-1)]:
+                        ni, nj = i+di, j+dj
+                        if 0 <= ni < n and 0 <= nj < n and grid[ni][nj] == 1:
+                            union((i,j), (ni,nj))
+
+        result = max(size.values(), default=0)
+
+        # flip 0
+        for i in range(n):
+            for j in range(n):
                 if grid[i][j] == 0:
-                    tempSize = 0
-                    parents = set()
-                    for di,dj in [(1,0),(0,1),(-1,0),(0,-1)]:
-                        ni,nj = di + i, dj + j
-                        if 0 <= ni < len(grid) and 0 <= nj < len(grid[0]) and grid[ni][nj] == 1:
-                            parents.add(find((ni,nj)))
-                    for par in parents:
-                        tempSize+=size[par]
-                    result = max(result, tempSize+1)
-        return result
+                    seen = set()
+                    temp = 1
+                    for di, dj in [(1,0),(0,1),(-1,0),(0,-1)]:
+                        ni, nj = i+di, j+dj
+                        if 0 <= ni < n and 0 <= nj < n and grid[ni][nj] == 1:
+                            root = find((ni,nj))
+                            if root not in seen:
+                                seen.add(root)
+                                temp += size[root]
+                    result = max(result, temp)
+
+        return result if result else n*n
+        
