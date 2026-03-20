0d2eaa533ab1f0794f3f60f784108bcbeecaa799
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-03-20 11:41:30 -0700

    Time: 2133 ms (5.04%) | Memory: 111.3 MB (5.03%) - LeetSync

---
+++ 854-making-a-large-island/making-a-large-island.py
@@ -0,0 +1,50 @@
+"""
+1,1
+1,0
+
+
+"""
+class Solution:
+    def largestIsland(self, grid: List[List[int]]) -> int:
+        parent = {}
+        size = {}
+        def find(cord):
+            if cord != parent[cord]:
+                parent[cord] = find(parent[cord])
+            return parent[cord]
+        def union(cord1, cord2):
+            p1 = find(cord1)
+            p2 = find(cord2)
+            parent[p1] = p2
+            size[p2]+=size[p1]
+            size.pop(p1)
+            return size[p2]
+        lands = deque()
+        for i in range(len(grid)):
+            for j in range(len(grid)):
+                if grid[i][j] == 1:
+                    parent[(i,j)] = (i,j)
+                    size[(i,j)] = 1
+                    lands.append((i,j))
+        result = 1
+        while lands:
+            i,j = lands.popleft()
+            for di,dj in [(1,0),(0,1),(-1,0),(0,-1)]:
+                ni,nj = di + i, dj + j
+                if 0 <= ni < len(grid) and 0 <= nj < len(grid[0]) and grid[ni][nj] == 1 and find((i,j)) != find((ni,nj)):
+                    result = max(result,union((i,j),(ni,nj)))
+        
+
+        for i in range(len(grid)):
+            for j in range(len(grid)):
+                if grid[i][j] == 0:
+                    tempSize = 0
+                    parents = set()
+                    for di,dj in [(1,0),(0,1),(-1,0),(0,-1)]:
+                        ni,nj = di + i, dj + j
+                        if 0 <= ni < len(grid) and 0 <= nj < len(grid[0]) and grid[ni][nj] == 1:
+                            parents.add(find((ni,nj)))
+                    for par in parents:
+                        tempSize+=size[find(par)]
+                    result = max(result, tempSize+1)
+        return result
