cf02bd79c94a1d9e69c9b7af1ce81ea8311595d9
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-03-23 11:58:24 -0700

    Time: 360 ms (42.59%) | Memory: 46.3 MB (56.45%) - LeetSync

--- 502-ipo/ipo.py
+++ 502-ipo/ipo.py
@@ -1,19 +1,21 @@
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-        proCaps = [(-p,c) for p,c in zip(profits,capital)]
-        proCaps.sort(key=lambda x:x[1])
+        proCaps = list(zip(capital, profits))
+        proCaps.sort()
+
         profitHeap = []
-        capitlHeap = []
         i = 0
-        while k>0:
-            while i < len(proCaps) and proCaps[i][1] <= w:
-                heapq.heappush(profitHeap,proCaps[i])
-                i+=1
-            if profitHeap:
-                popped = heapq.heappop(profitHeap)
-                w = w + popped[0]*-1
-                k-=1
-            else:
+
+        while k > 0:
+            while i < len(proCaps) and proCaps[i][0] <= w:
+                heapq.heappush(profitHeap, -proCaps[i][1])
+                i += 1
+
+            if not profitHeap:
                 return w
+
+            w += -heapq.heappop(profitHeap)
+            k -= 1
+
         return w
                 
