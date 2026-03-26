a4aae8e5d54449604bd6761507dcc26904bcbf82
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-03-26 15:42:15 -0700

    Time: 3 ms (90.46%) | Memory: 20.1 MB (18.00%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,18 +1,17 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        def myRec(arr,visited):
+        def REC(arr,counter):
             if len(arr) == len(nums):
-                if arr[:] not in res:
-                    res.append(arr[:])
+                result.append(arr.copy())
                 return
-            for i in range(len(nums)):
-                if visited[i] == False:
-                    arr.append(nums[i])
-                    visited[i] = True
-                    myRec(arr,visited)
+            for num in counter:
+                if counter[num] > 0:
+                    arr.append(num)
+                    counter[num]-=1
+                    REC(arr,counter)
+                    counter[num]+=1
                     arr.pop()
-                    visited[i] = False
-        res = []
-        myRec([],[False] * len(nums))
-        return res
-                
+        counter = Counter(nums)
+        result = []
+        REC([],counter)
+        return result
