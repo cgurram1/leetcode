a75578619ba42e2275feeeeeb0724f0ed19f4969
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-03-26 14:18:21 -0700

    Time: 3 ms (65.02%) | Memory: 19.6 MB (73.02%) - LeetSync

--- 1184-car-pooling/car-pooling.py
+++ 1184-car-pooling/car-pooling.py
@@ -1,23 +1,15 @@
 class Solution:
-    def carPooling(self, trips: List[List[int]], cap: int) -> bool:
-        pickup = [(x[1], x[2], x[0]) for x in trips]
-        heapq.heapify(pickup)
-        dropoff = []
-        while pickup:
-            while dropoff and dropoff[0][0] <= pickup[0][0]:
-                _, passengers = heapq.heappop(dropoff)
-                cap += passengers
-            start, end, passengers = heapq.heappop(pickup)
-            if passengers <= cap:
-                cap -= passengers
-                heapq.heappush(dropoff, (end, passengers))
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        trips.sort(key=lambda x:x[1])
+        dropOff = []
+        for trip in trips:
+            #Dropoff all before picking this
+            currPickUp = trip[1]
+            while dropOff and dropOff[0][0] <= currPickUp:
+                capacity+=heapq.heappop(dropOff)[1]
+            if capacity >= trip[0]:
+                capacity-=trip[0]
+                heapq.heappush(dropOff,(trip[2],trip[0]))
             else:
                 return False
-        while dropoff:
-            _, passengers = heapq.heappop(dropoff)
-            cap += passengers
-            
         return True
-
-
-            
