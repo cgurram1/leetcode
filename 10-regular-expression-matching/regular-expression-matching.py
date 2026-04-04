53ab68e9247e5ea2031bf3da0544908b158f5f2b
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-04-04 14:18:22 -0700

    Time: 3 ms (91.28%) | Memory: 21.5 MB (6.27%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.py
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -10,6 +10,7 @@ c*a*b
 
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
+        @lru_cache
         def fun(p1,p2):
             if p2 == len(p):
                 return p1 == len(s)
@@ -18,17 +19,10 @@ class Solution:
                     p2 += 2
                 return p2 == len(p)
             if p2 + 1 < len(p) and p[p2+1] == '*':
-                if p[p2] == '.':
-                    if fun(p1,p2+2) or fun(p1+1,p2):
-                        return True
-                else:
-                    i = p1
-                    if fun(i,p2+2):
-                        return True
-                    while i < len(s) and s[i] == p[p2]:
-                        if fun(i+1,p2+2):
-                            return True
-                        i+=1
+                if fun(p1, p2+2):
+                    return True
+                if p1 < len(s) and (s[p1] == p[p2] or p[p2] == '.'):
+                    return fun(p1+1, p2)
             elif s[p1] == p[p2] or p[p2] == '.':
                 if fun(p1+1,p2+1):
                     return True
