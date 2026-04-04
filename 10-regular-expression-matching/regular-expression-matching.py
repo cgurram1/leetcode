2871f2dbb875a208de75e863642ff67186d698de
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-04-04 14:09:09 -0700

    Time: 6045 ms (11.54%) | Memory: 19.5 MB (40.73%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -0,0 +1,40 @@
+"""
+p
+aab
+
+
+  p
+c*a*b
+
+"""
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        def fun(p1,p2):
+            if p2 == len(p):
+                return p1 == len(s)
+            if p1 == len(s):
+                while p2 + 1 < len(p) and p[p2+1] == '*':
+                    p2 += 2
+                return p2 == len(p)
+            # print(p1,p2)
+            if p2 + 1 < len(p) and p[p2+1] == '*':
+                if p[p2] == '.':
+                    for i in range(p1,len(s)+1):
+                        print("Hello",i)
+                        if fun(i,p2+2):
+                            return True
+                else:
+                    i = p1
+                    #pick 0 prev
+                    if fun(i,p2+2):
+                        return True
+                    while i < len(s) and s[i] == p[p2]:
+                        if fun(i+1,p2+2):
+                            return True
+                        i+=1
+            elif s[p1] == p[p2] or p[p2] == '.':
+                if fun(p1+1,p2+1):
+                    return True
+            return False
+        return fun(0,0)
