61c5665d7447c69f1e8985408dc512e8c856f3f5
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-04-22 13:10:43 -0700

    Time: 1994 ms (16.73%) | Memory: 34.9 MB (36.60%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -3,20 +3,11 @@ class Solution:
         s = sum(nums)
         if s % 2 != 0:
             return False
-        h = [[-1] * ((s//2)+1) for _ in range(len(nums)+1)]
-        def fun(index, remaining):
-            if remaining == 0:
-                return 1
-            if index == len(nums) or remaining < 0:
-                return 0
-            if h[index][remaining] != -1:
-                return h[index][remaining]
-            if fun(index + 1, remaining - nums[index]) == 1 or fun(index + 1, remaining) == 1:
-                h[index][remaining] = 1
-            else:
-                h[index][remaining] = 0
-            return h[index][remaining]
-        
-        if fun(0,s//2) == 1:
-            return True
-        return False
+        dp = [[False] * ((s//2)+1) for _ in range(len(nums)+1)]
+        dp[0][0] = True
+        for i in range(1,len(nums)+1):
+            for r in range(s//2+1):
+                dp[i][r] = dp[i-1][r]
+                if nums[i-1] <= r:
+                    dp[i][r] = dp[i][r] or dp[i-1][r - nums[i-1]]
+        return dp[len(nums)][s//2]
