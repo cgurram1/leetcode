3a2f4ee2fd5daea9a3b243d1432d9e8eacafd914
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-04-23 06:47:36 -0700

    Time: 2464 ms (37.41%) | Memory: 114.2 MB (72.79%) - LeetSync

---
+++ 1881-closest-subsequence-sum/closest-subsequence-sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minAbsDifference(self, nums: List[int], goal: int) -> int:
+        def fun(index,s,arr,res):
+            if index == len(arr):
+                res.append(s)
+                return
+            fun(index + 1,s + arr[index],arr,res)
+            fun(index + 1, s, arr,res)
+        res1,res2 = [],[]
+        fun(0,0,nums[:len(nums)//2],res1)
+        fun(0,0,nums[len(nums)//2:],res2)
+        res = float('inf')
+        res2 = sorted(res2)
+        for val in res1:
+            remain = goal - val
+            right = bisect_left(res2,remain)
+            if right < len(res2):
+                res = min(res, res2[right] - remain)
+            if right > 0:
+                res = min(res, remain - res2[right-1])
+        return res
+
