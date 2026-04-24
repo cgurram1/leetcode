572ed7802c7d9d399c559671a654df25bfa3846e
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-04-24 08:55:22 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (76.98%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,15 +1,21 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[-1 for _ in range(n+1)] for _ in range(m+1)]
-        def rec(r,c):
-            if r == m-1 and c == n - 1:
-                return 1
-            elif r > m-1 or c > n-1:
-                return 0
-            if dp[r][c] != -1:
-                return dp[r][c]
-            down = rec(r+1,c)
-            right = rec(r,c+1)
-            dp[r][c] = down + right
-            return dp[r][c]
-        return rec(0,0)
+        dp = [[0]*(m+1) for _ in range(n+1)]
+        dp[n-1][m-1] = 1
+        for i in range(m+1):
+            dp[n][i] = 0
+        for i in range(n+1):
+            dp[i][m] = 0
+        for i in range(n-1,-1,-1):
+            for j in range(m-1,-1,-1):
+                if i == n-1 and j == m-1:
+                    continue
+                dp[i][j] = dp[i+1][j] + dp[i][j+1]
+        return dp[0][0]
+        # def fun(i1,i2):
+        #     if i1 == m-1 and i2 == n-1:
+        #         return 1
+        #     if i1 >= m or i2 >= n:
+        #         return 0
+        #     return fun(i1+1,i2) + fun(i1,i2+1)
+        # return fun(0,0)
