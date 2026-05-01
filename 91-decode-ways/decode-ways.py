c17f022265b9c1edcde3b4857d38bdf87e079879
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-05-01 15:13:43 -0700

    Time: 31 ms (2.29%) | Memory: 19.3 MB (71.83%) - LeetSync

---
+++ 91-decode-ways/decode-ways.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = [-1 for _ in range(len(s)+1)]
+        def fun(index):
+            if index == len(s):
+                return 1
+            if dp[index] != -1:
+                return dp[index]
+            ans = 0
+            for i in range(index+1,len(s)+1):
+                if s[index] != '0' and 0 < int(s[index:i]) <= 26:
+                    ans+=fun(i)
+            dp[index] = ans
+            return ans
+        return fun(0)
