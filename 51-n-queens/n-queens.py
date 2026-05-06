984b229b3ded6cb6b72a20177aebd76b2860768b
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-05-06 11:29:15 -0700

    Time: 10 ms (73.62%) | Memory: 19.9 MB (10.13%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,40 +1,36 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        board = [['.']*n for _ in range(n)]
-        def canPlace(board,row,col):
-            for r in range(row):
-                if board[r][col] == 'Q':
-                    return False
-            r,c = row,col
-            r-=1
-            c-=1
-            while r >= 0 and c >= 0:
-                if board[r][c] == 'Q':
-                    return False
-                r-=1
-                c-=1
-            r,c = row,col
-            r-=1
-            c+=1
-            while r >= 0 and c < n:
-                if board[r][c] == 'Q':
-                    return False
-                r-=1
-                c+=1
-            return True
 
+        board = [['.'] * n for _ in range(n)]
         result = []
-        def placeQueen(board,remaining,n,row):
-            if remaining == 0:
-                return True
+
+        cols = set()
+        diag1 = set()
+        diag2 = set()
+
+        def backtrack(row):
+
+            if row == n:
+                result.append([''.join(r) for r in board])
+                return
+
             for col in range(n):
-                if canPlace(board, row,col):
-                    board[row][col] = 'Q'
-                    if placeQueen(board,remaining - 1,n,row+1):
-                        temp = []
-                        for r in board:
-                            temp.append(''.join(r))
-                        result.append(temp)
-                    board[row][col] = '.'
-        board = placeQueen(board,n,n,0)
+
+                if (col in cols or
+                    (row - col) in diag1 or
+                    (row + col) in diag2):
+                    continue
+                board[row][col] = 'Q'
+                cols.add(col)
+                diag1.add(row - col)
+                diag2.add(row + col)
+
+                backtrack(row + 1)
+                board[row][col] = '.'
+                cols.remove(col)
+                diag1.remove(row - col)
+                diag2.remove(row + col)
+
+        backtrack(0)
+
         return result
