dd7b4579be6b14325eaf6b9df21ef869ac9bad03
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-05-09 13:54:27 -0700

    Time: 0 ms (100.00%) | Memory: 19.6 MB (11.82%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,20 +1,21 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        m = len(obstacleGrid[0])
         n = len(obstacleGrid)
-        dp = [[0]*(m+1) for _ in range(n+1)]
-        if obstacleGrid[n-1][m-1] == 0:
-            dp[n-1][m-1] = 1
-        for i in range(n-1,-1,-1):
-            for j in range(m-1,-1,-1):
-                if obstacleGrid[i][j] == 1 or (i == n-1 and j == m-1):
-                    continue
-                dp[i][j] = dp[i+1][j] + dp[i][j+1]
-        return dp[0][0]
-        # def fun(i1,i2):
-        #     if i1 >= n or i2 >= m or obstacleGrid[i1][i2] == 1:
-        #         return 0
-        #     if i1 == n-1 and i2 == m-1:
-        #         return 1
-        #     return fun(i1+1,i2) + fun(i1,i2+1)
-        # return fun(0,0)
+        m = len(obstacleGrid[0])
+        if obstacleGrid[0][0] == 1 or obstacleGrid[n-1][m-1] == 1:
+            return 0
+        dirs = [(1,0),(0,1)]
+        dp = [[-1 for _ in range(m+1)] for _ in range(n+1)]
+        def fun(i,j):
+            if i == n-1 and j == m-1:
+                return 1
+            if dp[i][j] != -1:
+                return dp[i][j]
+            ans = 0
+            for di,dj in dirs:
+                ni,nj = i + di, j + dj
+                if 0 <= ni < n and 0 <= nj < m and obstacleGrid[ni][nj] != 1:
+                    ans += fun(ni,nj)
+            dp[i][j] = ans
+            return ans
+        return fun(0,0)
