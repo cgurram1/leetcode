10263d3d728513b21fb7e3d6599f04ecc76052e8
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-05-09 22:06:16 -0700

    Time: 123 ms (9.19%) | Memory: 29.2 MB (16.69%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.py
+++ 1002-maximum-width-ramp/maximum-width-ramp.py
@@ -1,14 +1,32 @@
+"""
+[0,1,2,3,4,5,6,7,8,9]
+[9,8,1,0,1,9,4,0,4,1]
+
+[(9,0),(8,1),(1,2),(0,3),()]
+
+
+currMax = 7
+"""
+
 class Solution:
     def maxWidthRamp(self, nums: List[int]) -> int:
+        result = 0
         stack = []
-        res = float('-inf')
         for i in range(len(nums)):
-            if not stack or nums[i] < nums[stack[-1]]:
-                stack.append(i)
-        for i in range(len(nums)-1,-1,-1):
-            while(stack and nums[stack[-1]] <= nums[i]):
-                popped = stack.pop(-1)
-            res = max(res,(i - popped))
-        if res == float('-inf'):
-            return 0
-        return res
+            if not stack or stack[-1][0] > nums[i]:
+                stack.append((nums[i],i))
+            else:
+                left = 0
+                right = len(stack)-1
+                while left <= right:
+                    mid = (left + right)//2
+                    if stack[mid][0] > nums[i]:
+                        left = mid + 1
+                    else:
+                        result = max(result, i - stack[mid][1])
+                        right = mid - 1
+        return result
+        
+        
+
+        # return max(0,result)
