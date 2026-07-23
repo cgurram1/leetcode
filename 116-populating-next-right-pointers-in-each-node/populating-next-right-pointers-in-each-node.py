bd48613c146d911a7558e16e00a325849a7deda4
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-07-22 19:37:35 -0700

    Time: 65 ms (16.15%) | Memory: 20.5 MB (96.24%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -10,27 +10,15 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        if root == None:
-            return root
-        queue = deque()
-        queue.append((root,None))
-        while queue:
-            direct = True
-            for _ in range(len(queue)):
-                node,parent = queue.popleft()
-                if node.left:
-                    queue.append((node.left,node))
-                    queue.append((node.right,node))
-                if parent == None:
-                    node.next = None
-                else:
-                    if direct == True:
+        def fun(node,isLeft,parent):
+            if node:
+                if parent:
+                    if isLeft:
                         node.next = parent.right
-                        direct = False
                     else:
                         if parent.next:
                             node.next = parent.next.left
-                        else:
-                            node.next = None
-                        direct = True
+                fun(node.left,True,node)
+                fun(node.right,False,node)
+        fun(root,True,None)
         return root
