a96043e49bea618baad06d608a904b426e837fa7
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-07-22 17:47:57 -0700

    Time: 71 ms (38.63%) | Memory: 24.6 MB (69.90%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,29 +1,11 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        myDict = {}
-        distances = set()
+        res = []
         for point in points:
-            x = point[0]
-            y = point[1]
-            dist = math.sqrt((x ** 2 ) + (y ** 2))
-            distances.add(dist)
-            if not dist in myDict:
-                myDict[dist] = [point]
-            else:
-                myDict[dist].append(point)
-        listDistances = list(distances)
-        heapq.heapify(listDistances)
-        count = 0
-        myArr = []
-        tempK = k
-        while(tempK > 0 and len(listDistances) > 0):
-            popped = heapq.heappop(listDistances)
-            curr = len(myDict[popped])
-            if count + curr <= k:
-                count += curr
-                myArr.extend(myDict[popped])
-            else:
-                remaining = k - count
-                myArr.extend(myDict[popped][:remaining])
-            tempK = tempK - 1
-        return myArr
+            heapq.heappush(res,(-1*math.sqrt(point[0]**2 + point[1]**2),point))
+            if len(res) > k:
+                heapq.heappop(res)
+        ans = []
+        while res:
+            ans.append(heapq.heappop(res)[1])
+        return ans
