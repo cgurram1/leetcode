71dee36a1c561cd44d93365d90301ffdea6e3bdd
cgurram1 <47153577+cgurram1@users.noreply.github.com>
2026-07-23 11:34:36 -0700

    Time: 390 ms (11.52%) | Memory: 19.5 MB (16.87%) - LeetSync

--- 843-binary-trees-with-factors/binary-trees-with-factors.py
+++ 843-binary-trees-with-factors/binary-trees-with-factors.py
@@ -1,17 +1,14 @@
 class Solution:
     def numFactoredBinaryTrees(self, arr):
         MOD = 10**9 + 7
-
         arr.sort(reverse=True)
-
         m = {}
+        ans = 0
         for a in arr:
             m[a] = 1
-
         for i in range(len(arr) - 1, -1, -1):
             left = i + 1
             right = len(arr) - 1
-
             while left <= right:
                 product = arr[left] * arr[right]
                 if product == arr[i]:
@@ -22,14 +19,13 @@ class Solution:
                     m[arr[i]] %= MOD
                     left += 1
                     right -= 1
+                
                 elif product < arr[i]:
                     right -= 1
-
                 else:
                     left += 1
-
-        answer = 0
-        for value in m.values():
-            answer = (answer + value) % MOD
-
-        return answer
+            ans = (ans + m[arr[i]]) % MOD
+        # answer = 0
+        # for value in m.values():
+        #     answer = (answer + value) % MOD
+        return ans
